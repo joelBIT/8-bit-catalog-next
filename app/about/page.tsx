1ebfed82bb6e82e0080ca114dccea9cde4c7ba72
joelBit <joel.rollny@gmail.com>
2024-10-28 08:37:09 +0100

    Show active link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import { FieldSetFrame } from "@/components/common/FieldSetFrame";
 import { ReactElement } from "react";
 import styles from "./about.module.css";
+import { silkScreen } from "@/fonts/fonts";
 
 export default function AboutPage(): ReactElement {
     return (
@@ -13,11 +14,11 @@ export default function AboutPage(): ReactElement {
 function Text(): ReactElement {
     return (
         <>
-            <p className={styles.p}>
+            <p className={`${styles.p} ${silkScreen.className}`}>
                 The purpose of the 8-bit Catalog is to be an as accomplished source of NES games and ROMS as possible.
                 This catalog is continuously updated with new information.
             </p>
-            <p className={styles.p}>
+            <p className={`${styles.p} ${silkScreen.className}`}>
                 It is possible to submit requests for games that are missing in the catalog. After being reviewed
                 by an admin a request can be either denied or accepted. If accepted, the game will be added to
                 the catalog. A submitter will get feedback on a denied request in case the request was created while being authenticated.
--- components/footer/SiteLinks.tsx
+++ components/footer/SiteLinks.tsx
@@ -1,25 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import { ReactElement } from "react";
 import styles from "./sitelinks.module.css";
+import { usePathname } from "next/navigation";
 
 export function SiteLinks(): ReactElement {
+    const pathname = usePathname();
 
     return (
         <section id={styles.siteLinks}>
             <h2 className={styles.h2}>Site Links</h2>
             <ul className={styles.ul}>
                 <li>
-                    <Link href="/" className={styles.link}>
+                    <Link href="/" className={pathname === "/" ? `active ${styles.link}` : `${styles.link}`}>
                         <h4 className={styles.h4}>Home</h4>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/about" className={styles.link}>
+                    <Link href="/about" className={pathname === "/about" ? `active ${styles.link}` : `${styles.link}`} >
                         <h4 className={styles.h4}>About</h4>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/account" className={styles.link}>
+                    <Link href="/account" className={pathname === "/account" ? `active ${styles.link}` : `${styles.link}`}>
                         <h4 className={styles.h4}>Account</h4>
                     </Link>
                 </li>
