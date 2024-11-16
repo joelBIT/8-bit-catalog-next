0339e663880a469450335be27f4abc66928e7932
joelBit <joel.rollny@gmail.com>
2024-11-16 13:30:06 +0100

    map array in footer links

--- components/footer/SiteLinks.tsx
+++ components/footer/SiteLinks.tsx
@@ -9,25 +9,25 @@ import "./SiteLinks.css";
 export function SiteLinks(): ReactElement {
     const pathname = usePathname();
 
+    const SITELINKS = [
+        {path: "/", title: "Home"},
+        {path: "/about", title: "About"},
+        {path: "/login", title: "Account"}
+    ];
+
     return (
         <section id="siteLinks">
             <h2 className="siteLinks__title">Site Links</h2>
             <ul className="siteLinks__list">
-                <li>
-                    <Link href="/" className={pathname === "/" ? `active siteLinks__link` : `siteLinks__link`}>
-                        <h4 className="siteLinks__link-title">Home</h4>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about" className={pathname === "/about" ? `active siteLinks__link` : `siteLinks__link`} >
-                        <h4 className="siteLinks__link-title">About</h4>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/login" className={pathname === "/login" ? `active siteLinks__link` : `siteLinks__link`}>
-                        <h4 className="siteLinks__link-title">Account</h4>
-                    </Link>
-                </li>
+                {
+                    SITELINKS.map(link => 
+                        <li>
+                            <Link href={link.path} className={pathname === link.path ? `active siteLinks__link` : `siteLinks__link`}>
+                                <h4 className="siteLinks__link-title"> {link.title} </h4>
+                            </Link>
+                        </li>
+                    )
+                }
             </ul>
         </section>
     );
