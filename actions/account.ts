6f71eee386cd5783142bd69886075d6bb4a322d1
joelBit <joel.rollny@gmail.com>
2024-12-01 09:24:51 +0100

    Enabled register and login using supabase auth

--- actions/account.ts
+++ actions/account.ts
@@ -1,23 +1,68 @@
 'use server';
 
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 export async function login(prevState: any, formData: FormData) {
-    const rawFormData = {
-        username: formData.get('username'),
-        password: formData.get('password')
+    const supabase = await createClient();
+    
+    const loginData = {
+        username: formData.get('username') as string,
+        password: formData.get('password') as string
     };
-    console.log(rawFormData, prevState);
+    console.log(loginData, prevState);
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: loginData.username,
+        password: loginData.password
+    });
+
+    console.log(data);  
+
+    if (error) {
+        //redirect('/error');
+        console.log(error);
+    }
+
+    revalidatePath('/', 'layout');
+    redirect('/');
 
     return { message: 'Logged in' };
 }
 
 export async function register(prevState: any, formData: FormData) {
-    const rawFormData = {
-        username: formData.get('username'),
-        password: formData.get('password'),
-        passwordRepeat: formData.get('passwordRepeat'),
-        email: formData.get('email')
+    const supabase = await createClient();
+
+    const registerData = {
+        username: formData.get('username') as string,
+        password: formData.get('password') as string,
+        passwordRepeat: formData.get('passwordRepeat') as string,
+        email: formData.get('email') as string
     };
-    console.log(rawFormData, prevState);
+    console.log(registerData, prevState);
+
+    const { data, error } = await supabase.auth.signUp(
+        {
+          email: registerData.email,
+          password: registerData.password,
+          options: {
+            data: {
+              user_name: registerData.username
+            }
+          }
+        }
+    );
+
+    console.log(data);
+
+    if (error) {
+      //redirect('/error');
+      console.log(error);
+    }
+  
+    revalidatePath('/', 'layout');
+    redirect('/');
 
     return { message: 'Registered' };
 }
