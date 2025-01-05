875f394805fb5958d8eb0c23a17ce6883f44d46a
joelBit <joel.rollny@gmail.com>
2025-01-05 12:40:29 +0100

    Made edit game page a server component

--- app/gamedetails/[id]/edit/page.tsx
+++ app/gamedetails/[id]/edit/page.tsx
@@ -1,28 +1,15 @@
-'use client';
-
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import { FieldSetFrame } from "@/components/common/FieldSetFrame";
 import { EditGameForm } from "@/components/gamedetails/edit/EditGameForm";
-import { Game } from "@/interfaces/interfaces";
+import { getGameById } from "@/db/db";
 
 import "./page.css";
 
-export default function EditGamePage({ params }: { params: { id: string }}): ReactElement {
-    const [ game, setGame ] = useState<Game>();
-
-    useEffect(() => {
-        const fetchGame = async () => {
-            const response = await fetch(`/api/game?id=${params.id}`);
-            const data = await response.json();
-            setGame(data);
-        }
-
-        fetchGame();
-    }, []);
+export default async function EditGamePage({ params }: { params: { id: string }}): Promise<ReactElement> {
     
     return (
         <main id="editGamePage">
-            { game ? <FieldSetFrame legend={"Edit Details"} body={<EditGameForm game={game} />} /> : <></> }
+            <FieldSetFrame legend={"Edit Details"} body={<EditGameForm game={ await getGameById(parseInt(params.id)) } />} />
         </main>
     );
 }
