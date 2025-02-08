2ab2fcc917f100a45fb5e062479b2f9d00dc5148
joelBit <joel.rollny@gmail.com>
2025-02-08 16:02:05 +0100

    Added try catch for filters

--- app/api/developers/route.ts
+++ app/api/developers/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 import { getAllDevelopers } from "@/db/db";
 
 export async function GET() {
-    const developers = await getAllDevelopers();
-    return NextResponse.json(developers);
+    try {
+        const developers = await getAllDevelopers();
+        return NextResponse.json(developers);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return NextResponse.json({ error: true, message: "Could not retrieve developers" }, { status: 500 });
 }
--- app/api/publishers/route.ts
+++ app/api/publishers/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 import { getAllPublishers } from "@/db/db";
 
 export async function GET() {
-    const publishers = await getAllPublishers();
-    return NextResponse.json(publishers);
+    try {
+        const publishers = await getAllPublishers();
+        return NextResponse.json(publishers);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return NextResponse.json({ error: true, message: "Could not retrieve publishers" }, { status: 500 });
 }
