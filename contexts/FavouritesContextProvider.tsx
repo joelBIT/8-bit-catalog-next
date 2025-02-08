a736620a2244964ade6a2b392f433fc987e09c4e
joelBit <joel.rollny@gmail.com>
2025-02-08 12:07:42 +0100

    Use localstorage if available when adding or removing favourite games

--- contexts/FavouritesContextProvider.tsx
+++ contexts/FavouritesContextProvider.tsx
@@ -1,33 +1,60 @@
 'use client';
 
-import { createContext, ReactElement, useState } from "react";
+import { createContext, ReactElement, useEffect, useState } from "react";
 import { FavouritesContextProvider, ContextProviderChildren } from "@/interfaces/interfaces";
 import { Game } from "@/types/types";
 import { isLocalStorageAvailable } from "@/utils/utils";
 
 export const FavouritesContext = createContext<FavouritesContextProvider>({} as FavouritesContextProvider);
 
+/**
+ * Favourite games are placed in localstorage if localstorage is available. If not available, the favourite games
+ * are only stored in this Context's favouritesList variable.
+ */
 export function FavouritesContexProvider({ children }: ContextProviderChildren): ReactElement {
     const [ favouritesList, setFavouritesList ] = useState<Game[]>([]);
+    const STORAGE_KEY = 'favouriteGames';
 
-    function storeFavouriteGames(favouritesList: Game[]): void {
+    /**
+     * Load favourite games from localstorage if localstorage is available.
+     */
+    useEffect(() => {
         if (isLocalStorageAvailable()) {
-            localStorage.setItem('favouriteGames', JSON.stringify(favouritesList));
+            if (localStorage.getItem(STORAGE_KEY)) {
+                setFavouritesList(JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'));
+            } else {
+                localStorage.setItem(STORAGE_KEY, '[]');
+            }
+        }
+    }, []); 
+
+    /**
+     * Adds a game as a favourite in localstorage if localstorage is available.
+     */
+    function addFavouriteGame(game: Game): void {
+        if (isLocalStorageAvailable()) {
+            setFavouritesList([...favouritesList, game]);
+            localStorage.setItem(STORAGE_KEY, JSON.stringify([...favouritesList, game]));
         } else {
-            setFavouritesList(favouritesList);
+            setFavouritesList([...favouritesList, game]);
         }
     }
 
-    function getFavouriteGames(): Game[] {
+    /**
+     * Removes a game from the list of favourites in localstorage if localstorage is available.
+     */
+    function removeFavouriteGame(game: Game): void {
         if (isLocalStorageAvailable()) {
-            return JSON.parse(localStorage.getItem('favouriteGames') || '');
+            const games = favouritesList.filter((favourite: { id: number; }) => favourite.id !== game.id);
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(games));
+            setFavouritesList(games);
         } else {
-            return favouritesList;
+            setFavouritesList(favouritesList.filter((favourite: { id: number; }) => favourite.id !== game.id));
         }
     }
 
     return (
-        <FavouritesContext.Provider value={{ getFavouriteGames, storeFavouriteGames }}>
+        <FavouritesContext.Provider value={{ favouritesList, addFavouriteGame, removeFavouriteGame }}>
             { children }
         </FavouritesContext.Provider>
     );
--- interfaces/interfaces.tsx
+++ interfaces/interfaces.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from "react";
 import { Game } from "@/types/types";
 
 export interface FavouritesContextProvider {
-    getFavouriteGames: () => Game[];
-    storeFavouriteGames: (favouritesList: Game[]) => void;
+    favouritesList: Game[],
+    addFavouriteGame: (game: Game) => void;
+    removeFavouriteGame: (game: Game) => void;
 }
 
 export interface ContextProviderChildren {
