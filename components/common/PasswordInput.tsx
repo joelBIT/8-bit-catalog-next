92f8f0e8958f64ca08bc973c805747d7e2085a02
joelBit <joel.rollny@gmail.com>
2025-03-05 13:24:38 +0100

    Creaed PasswordInput component

---
+++ components/common/PasswordInput.tsx
@@ -0,0 +1,25 @@
+import { arima } from "@/fonts/fonts";
+import { ReactElement, useState } from "react";
+
+import "./PasswordInput.css";
+
+export function PasswordInput({ id, placeholder }: { id: string, placeholder: string }): ReactElement<ReactElement> {
+    const [isVisible, setVisible] = useState(false);
+    
+    return (
+        <div className="password-input">
+        <input 
+            id={id}
+            name={id} 
+            type={isVisible ? "text" : "password"}
+            placeholder={placeholder}
+            className={arima.className}
+            autoComplete="false" 
+            required 
+        />
+        <span className="material-symbols-outlined" onClick={() => setVisible(!isVisible)}>
+        {isVisible ? "visibility_off" : "visibility"}
+      </span>
+      </div>
+    );
+}
