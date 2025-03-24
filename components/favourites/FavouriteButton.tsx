af1b69e3ca1d951615045f7a1e615253a8cc5c4c
joelBit <joel.rollny@gmail.com>
2025-03-24 10:21:39 +0100

    Game card fades out when removed from favourites page

--- components/favourites/FavouriteButton.tsx
+++ components/favourites/FavouriteButton.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { ReactElement, useContext } from "react";
+import { usePathname } from 'next/navigation'
 import { Game } from "@/types/types";
 import { FavouritesContext } from "@/contexts/FavouritesContextProvider";
 
 import "./FavouriteButton.css";
 
-export function FavouriteButton({ game }: { game: Game }): ReactElement<ReactElement> {
+export function FavouriteButton({ game, setFading }: { game: Game, setFading: (fading: boolean) => void }): ReactElement<ReactElement> {
+    const pathname = usePathname();
     const { favouritesList, addFavouriteGame, removeFavouriteGame } = useContext(FavouritesContext);
     const isFavorite = favouritesList.some((favourite: { id: number; }) => favourite.id === game.id);
 
@@ -18,7 +20,13 @@ export function FavouriteButton({ game }: { game: Game }): ReactElement<ReactEle
         event.preventDefault();
 
         if (isFavorite) {
-            removeFavouriteGame(game);
+            if (pathname === "/favourites") {           // If a game card is removed from favourites page, it fades out
+                setFading(true);
+                setTimeout(() => removeFavouriteGame(game), 500);
+                setTimeout(() => setFading(false), 600);
+            } else {
+                removeFavouriteGame(game);
+            }
         } else {
             addFavouriteGame(game);
         }
