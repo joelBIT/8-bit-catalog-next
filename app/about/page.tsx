920a9cec0e37814a0c2a554b03a45f1cb2eabd36
joelBit <joel.rollny@gmail.com>
2025-04-02 15:56:29 +0200

    Changed font for about text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { FieldSetFrame } from "@/components/common/FieldSetFrame";
-import { silkScreen } from "@/fonts/fonts";
 
 import "./page.css";
 
@@ -15,11 +14,11 @@ export default function AboutPage(): ReactElement<ReactElement> {
 function Text(): ReactElement<ReactElement> {
     return (
         <>
-            <p className={`aboutPage__paragraph ${silkScreen.className}`}>
+            <p className={`aboutPage__paragraph bit-font`}>
                 The purpose of the 8-bit Catalog is to be an as accomplished source of NES games as possible.
                 This catalog is continuously updated with new information.
             </p>
-            <p className={`aboutPage__paragraph ${silkScreen.className}`}>
+            <p className={`aboutPage__paragraph bit-font`}>
                 Games that have a mapper implemented by emulator.joel-rollny.eu can be played in a browser.
                 An increasing number of games will be supported over time due to the emulator being an ongoing project.
             </p>
