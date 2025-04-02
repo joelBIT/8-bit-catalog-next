b02b4f6443c311e9cd17a0450c30c293d9c2cc3b
joelBit <joel.rollny@gmail.com>
2025-04-02 09:32:22 +0200

    Refresh favourites when multiple tabs open

--- app/forbidden/page.tsx
+++ app/forbidden/page.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from "next/navigation";
+import { FavouritesContext } from '@/contexts/FavouritesContextProvider';
 
 import "./page.css";
  
 export default function ForbiddenPage() {
+    const { loadFavouriteGames } = useContext(FavouritesContext);
     const router = useRouter();
 
     useEffect(() => {
-        router.refresh();       // Refresh page so that Header is updated accordingly
+        loadFavouriteGames();   // Update the favourites since user might be having multiple tabs open
+        router.refresh();       // Refresh page so that Header is updated accordingly when multiple tabs are open
     }, []);
 
     return (
