1d15ed99aa7e088f91324fc05a4799d3435a602e
joelBit <joel.rollny@gmail.com>
2025-04-05 14:31:44 +0200

    Added comments to utils.ts

--- app/gamedetails/[id]/page.tsx
+++ app/gamedetails/[id]/page.tsx
@@ -7,11 +7,11 @@ import "./page.css";
 
 export default async function GameDetailsPage({ params }: { params: Promise<{ id: string }> }): Promise<ReactElement<ReactElement>> {
     const param = await params;
-    const id = param.id;
+    const id = parseInt(param.id);
 
     return (
         <main id="gameDetailsPage">
-            <FieldSetFrame legend="Game Details" body={<GameDetailsCard game={ await getGameById(parseInt(id)) }/>} />
+            <FieldSetFrame legend="Game Details" body={<GameDetailsCard game={ await getGameById(id) }/>} />
         </main>
     );
 }
