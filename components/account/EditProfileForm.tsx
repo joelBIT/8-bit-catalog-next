874f380ae7082f850c1585995d45a37e6d8f4c3e
joelBit <joel.rollny@gmail.com>
2025-04-09 08:51:39 +0200

    Adjusted profile account page for mobile

--- components/account/EditProfileForm.tsx
+++ components/account/EditProfileForm.tsx
@@ -16,7 +16,11 @@ export function EditProfileForm(): ReactElement {
         <form id="editProfileForm" action={formAction}>
             <section className="edit-profile-image">
                 <img src={state.image ? STORAGE_URL + state.image : STORAGE_URL + user?.image} className="profile-image" alt="Profile image" />
-                <h2 className="edit-profile__change-image"> Change profile image <input name="profileImage" type="file" accept={imageTypes.toString()} />  </h2>
+
+                <h2 className="edit-profile__change-image"> 
+                    Change profile image 
+                    <input name="profileImage" className="edit-profile__input" type="file" accept={imageTypes.toString()} />
+                </h2>
             </section>
 
             <button className="gameButton" type="submit"> Save </button>
