4709ab9b28485119d813d1290f58f61eca20a23b
joelBit <joel.rollny@gmail.com>
2025-04-12 17:13:51 +0200

    Possible to toggle between grid and list view in favourites

--- app/(account)/favourites/page.tsx
+++ app/(account)/favourites/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useState } from "react";
 import { PAGINATION_PAGE_SIZE } from "@/utils/utils";
 import { FavouritePagination } from "@/components/favourites/FavouritePagination";
 import { FavouritesContext } from "@/contexts/FavouritesContextProvider";
-import { GameCard } from "@/components/common";
+import { GameCard, ListToggle } from "@/components/common";
 import { arima } from "@/fonts/fonts";
 
 import "./page.css";
 
 export default function FavouritesPage(): ReactElement {
     const { favouritesList, favouritesPage } = useContext(FavouritesContext);
+    const [ gridView, setGridView ] = useState<boolean>(true);
 
     function from(page: number): number {
         return (page-1) * PAGINATION_PAGE_SIZE;
@@ -20,13 +21,20 @@ export default function FavouritesPage(): ReactElement {
         return (page-1) * PAGINATION_PAGE_SIZE + PAGINATION_PAGE_SIZE;
     }
 
+    function toggleGridView() {
+        setGridView(!gridView);
+    }
+
     return (
         <main id="favouritesPage">
             {
                 favouritesList.length > 0 ?
-                    <h1 className={`${arima.className}`}>
-                        {favouritesList.length} favourite games 
-                    </h1>
+                    <>
+                        <h1 className={`number-favourites-text ${arima.className}`}>
+                            {favouritesList.length} favourite games 
+                        </h1>
+                        <ListToggle toggle={toggleGridView} />
+                    </>
                     : <></>
             }
 
@@ -36,7 +44,7 @@ export default function FavouritesPage(): ReactElement {
                 { 
                     favouritesList.length === 0 ? 
                         <h1 className="no-favourites-text"> No favourites selected </h1> :
-                        favouritesList.slice(from(favouritesPage), to(favouritesPage)).map(game => <GameCard key={game.id} game={game} grid={true} />) 
+                        favouritesList.slice(from(favouritesPage), to(favouritesPage)).map(game => <GameCard key={game.id} game={game} grid={gridView} />) 
                 }
             </section>
 
