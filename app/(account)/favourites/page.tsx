acc69a400017212a4a66e50164842f29fddafc95
joelBit <joel.rollny@gmail.com>
2025-04-12 16:40:22 +0200

    Show number of favourite games

--- app/(account)/favourites/page.tsx
+++ app/(account)/favourites/page.tsx
@@ -5,10 +5,11 @@ import { PAGINATION_PAGE_SIZE } from "@/utils/utils";
 import { FavouritePagination } from "@/components/favourites/FavouritePagination";
 import { FavouritesContext } from "@/contexts/FavouritesContextProvider";
 import { GameCard } from "@/components/common";
+import { arima } from "@/fonts/fonts";
 
 import "./page.css";
 
-export default function FavouritesPage(): ReactElement<ReactElement> {
+export default function FavouritesPage(): ReactElement {
     const { favouritesList, favouritesPage } = useContext(FavouritesContext);
 
     function from(page: number): number {
@@ -21,12 +22,22 @@ export default function FavouritesPage(): ReactElement<ReactElement> {
 
     return (
         <main id="favouritesPage">
+            {
+                favouritesList.length > 0 ?
+                    <h1 className={`${arima.className}`}>
+                        {favouritesList.length} favourite games 
+                    </h1>
+                    : <></>
+            }
+
             {favouritesList.length > PAGINATION_PAGE_SIZE ? <FavouritePagination currentPage={favouritesPage} /> : <></>}
 
             <section id="gameCards">
-                { favouritesList.length === 0 ? 
-                    <h1 className="no-favourites-text">{"No favourites selected"}</h1> : 
-                    favouritesList.slice(from(favouritesPage), to(favouritesPage)).map(game => <GameCard key={game.id} game={game} grid={true} />) }
+                { 
+                    favouritesList.length === 0 ? 
+                        <h1 className="no-favourites-text"> No favourites selected </h1> :
+                        favouritesList.slice(from(favouritesPage), to(favouritesPage)).map(game => <GameCard key={game.id} game={game} grid={true} />) 
+                }
             </section>
 
             {favouritesList.length > PAGINATION_PAGE_SIZE ? <FavouritePagination currentPage={favouritesPage} /> : <></>}
