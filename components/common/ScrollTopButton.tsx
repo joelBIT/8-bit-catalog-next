8bfdbabdae27984235efbfd8fb337b3558d88304
joelBit <joel.rollny@gmail.com>
2025-04-17 11:51:00 +0200

    ScrollTopButton is only visible scrolling upwards when view is below a certain y position

--- components/common/ScrollTopButton.tsx
+++ components/common/ScrollTopButton.tsx
@@ -1,10 +1,36 @@
-import { ReactElement } from "react";
+'use client';
+
+import { ReactElement, useEffect, useState } from "react";
 
 import "./ScrollTopButton.css";
 
+/**
+ * This component only appear on screen after a certain y position and when scrolling upwards. When a user clicks on this
+ * component the window scrolls to top.
+ */
 export function ScrollTopButton(): ReactElement {
+    const [ isVisible, setIsVisible ] = useState<boolean>(false);
+    let position = 0;
 
-    function scrollToTop() {
+    useEffect(() => {
+        window.addEventListener("scroll", scroll, false);
+
+        return () => {
+            window.removeEventListener("scroll", scroll, false);
+        };
+    }, [])
+
+    function scroll(): void {
+        if ((position > 1500) && !isVisible && (window.scrollY < position)) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+        
+        position = window.scrollY;
+    }
+
+    function scrollToTop(): void {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
@@ -12,8 +38,15 @@ export function ScrollTopButton(): ReactElement {
     }
 
     return (
-        <section id="scrollTopButton-wrapper" onClick={scrollToTop}>
-            <span className="material-symbols-outlined"> keyboard_arrow_up </span>
-        </section>
+        <>
+            {
+                isVisible ?
+                    <section id="scrollTopButton-wrapper" onClick={scrollToTop}>
+                        <span className="material-symbols-outlined"> keyboard_arrow_up </span>
+                    </section>
+                    : <></>
+
+            }
+        </>
     );
 }
