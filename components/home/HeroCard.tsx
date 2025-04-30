c236005681ec7f886bed25898afb0b42755a786a
joelBit <joel.rollny@gmail.com>
2025-04-30 11:19:25 +0200

    Changed appearance of hero card

--- components/home/HeroCard.tsx
+++ components/home/HeroCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from 'next/image';
 import { Game } from "@/types/types";
 import { URL_GAME_DETAILS_PAGE } from "@/utils/utils";
-import { silkScreen, singleDay } from "@/fonts/fonts";
+import { singleDay } from "@/fonts/fonts";
 
 import "./HeroCard.css";
 
@@ -12,27 +12,22 @@ export function HeroCard({ game }: { game: Game }): ReactElement {
 
     return (
         <section id="heroCard" className="pixel-corners">
-            <article className="heroCard-wrapper">
-                <h1 className={`heroCard__title ${silkScreen.className}`}> {game.title} </h1>
-                
-                <Link 
-                    href={`${URL_GAME_DETAILS_PAGE}/${game.id}`} 
-                    className={`heroCard__link ${singleDay.className}`}
-                >
-                    View Game
-                </Link>
-            </article>
-            
-            <figure className="heroCard__figure">
-                <Image 
-                    src={STORAGE_URL + game.cover}
-                    unoptimized
-                    className="heroCard__figure-cover"
-                    alt="Random Game Cover"
-                    width={100}
-                    height={300}
-                />
-            </figure>
+            <Link href={`${URL_GAME_DETAILS_PAGE}/${game.id}`} className='heroCard__link'>
+
+                <article className="heroCard-wrapper pixel-corners">
+                    <Image 
+                        src={STORAGE_URL + game.cover}
+                        unoptimized
+                        className="heroCard__figure-cover"
+                        alt="Random Game Cover"
+                        width={768}
+                        height={300}
+                        title={game.title}
+                    />
+                </article>
+
+                <h1 className={`heroCard__title ${singleDay.className}`}> {game.title} </h1>
+            </Link>
         </section>
     );
 }
