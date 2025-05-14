9394fa76020f3b73a8a4d4caa3dc46554518a650
joelBit <joel.rollny@gmail.com>
2025-05-14 10:57:08 +0200

    Created grid layout on register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,12 +1,25 @@
 import { ReactElement } from "react";
+import Image from 'next/image';
 import { RegisterForm } from "@/components/auth/RegisterForm";
+import image from "../../../assets/backgrounds/nes-gaming-set-in-dark.webp";
 
 import "./page.css";
 
 export default function RegisterPage(): ReactElement {
     return (
         <main id="registerPage">
-            <RegisterForm />
+            <section id="registerPage-image">
+                <Image 
+                    src={image}
+                    unoptimized
+                    className="registerPage-nes-image"
+                    alt="NES gaming set"
+                />
+            </section>
+
+            <section id="registerPage-form"> 
+                <RegisterForm />
+            </section>
         </main>
     );
 }
