d5a5ce0b5cb8f130153e3dc8405dbc96a9496c24
joelBit <joel.rollny@gmail.com>
2025-05-16 11:18:21 +0200

    Made color variable for link background

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${blinker.className}`}>
-                    {children} 
+                {children} 
             </body>
         </html>
     );
