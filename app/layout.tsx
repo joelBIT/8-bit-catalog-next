3df3f9832e0b9812143f00dee492851edcbca23a
joelBit <joel.rollny@gmail.com>
2025-05-16 10:47:16 +0200

    Moved layout from auth to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { blinker } from "@/fonts/fonts";
 
-import "../globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
     title: "The 8-bit Catalog",
@@ -16,7 +16,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${blinker.className}`}>
-                {children}
+                    {children} 
             </body>
         </html>
     );
