5eeaa4b91569474b49d57662a357964e2415c1c4
joelBit <joel.rollny@gmail.com>
2025-05-21 11:07:05 +0200

    Added svg for landing page title

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,12 +12,20 @@ export default async function Home(): Promise<ReactElement> {
     return (
         <main id="landingPage">
             <section id="image-wrapper">
+                <svg viewBox="0 0 30 3">
+                    <path id="landing-title" fill="none" d="M 0 5 Q 15 -5 30 5" pathLength="2" />
+                    <text id="landing-text" fill="#f0cca4" className={`${irishGrover.className}`} font-size="2" dominant-baseline="hanging" text-anchor="middle">
+                        <textPath href="#landing-title" startOffset="1">
+                            The 8-bit Catalog
+                        </textPath>
+                    </text>
+                </svg>
+
                 <Logo />
-                <h1 className={`landing-text ${irishGrover.className}`}> The 8-bit Catalog </h1>
             </section>
 
             <section id="content">
-                
+
             </section>
         </main>
     );
