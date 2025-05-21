e10af157d38b7aa66af6bfa967e782b429cc73dd
joelBit <joel.rollny@gmail.com>
2025-05-21 16:01:53 +0200

    Sort timeline by year

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home(): Promise<ReactElement> {
             <section id="image-wrapper">
                 <svg viewBox="0 0 30 3">
                     <path id="landing-title" fill="none" d="M 0 5 Q 15 -5 30 5" pathLength="2" />
-                    <text id="landing-text" fill="#f0cca4" className={`${irishGrover.className}`} font-size="2" dominant-baseline="hanging" text-anchor="middle">
+                    <text id="landing-text" fill="#f0cca4" className={`${irishGrover.className}`} fontSize="2" dominantBaseline="hanging" textAnchor="middle">
                         <textPath href="#landing-title" startOffset="1">
                             The 8-bit Catalog
                         </textPath>
