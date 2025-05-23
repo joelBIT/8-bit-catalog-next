3f5d22a0841fe2074ebba6cb7ab64ae50db2bb17
joelBit <joel.rollny@gmail.com>
2025-05-23 10:30:13 +0200

    Added favourite button to game list

--- app/_components/common/lists/GameList.tsx
+++ app/_components/common/lists/GameList.tsx
@@ -1,15 +1,19 @@
-import { ReactElement } from "react";
+'use client';
+
+import { ReactElement, useState } from "react";
 import Image from 'next/image';
 import { Game } from "@/app/_types/types";
+import { FavouriteButton } from "../../favourites";
 
 import "./GameList.css";
 
 export function GameList({ games }: { games: Game[] }): ReactElement {
+    const [ isFadingOut, setIsFadingOut ] = useState<boolean>(false);
+    const [ removeCard, setRemoveCard ] = useState<boolean>(false);
     const STORAGE_URL = process.env.NEXT_PUBLIC_COVER;
 
     return (
         <ul id="gameList">
-            
             {
                 games.map(game => 
                     <li key={game.id} className="gameList-row">
@@ -19,11 +23,15 @@ export function GameList({ games }: { games: Game[] }): ReactElement {
                             className="gameCard-figure__cover"
                             alt="Game Cover"
                             width={100}
-                            height={300}
+                            height={100}
                         />
 
                         <h2 className="gameCard-title"> {game.title} </h2>
                         <h2 className="gameCard-category"> {game.category} </h2>
+                        <h2 className="gameCard-players"> {game.players} </h2>
+                        <h2 className="gameCard-developer"> {game.developer} </h2>
+                        <h2 className="gameCard-publisher"> {game.publisher} </h2>
+                        <FavouriteButton game={game} setFading={setIsFadingOut} removeCard={setRemoveCard} />
                     </li>
                 )
             }
