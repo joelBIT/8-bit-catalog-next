8c58eef5cc1ee67247251a589e26de0601529aca
joelBit <joel.rollny@gmail.com>
2025-05-23 09:50:48 +0200

    Added GameList to favourites page

--- app/_components/common/lists/GameList.tsx
+++ app/_components/common/lists/GameList.tsx
@@ -1,15 +1,29 @@
 import { ReactElement } from "react";
+import Image from 'next/image';
 import { Game } from "@/app/_types/types";
 
 import "./GameList.css";
 
 export function GameList({ games }: { games: Game[] }): ReactElement {
+    const STORAGE_URL = process.env.NEXT_PUBLIC_COVER;
+
     return (
         <ul id="gameList">
+            
             {
                 games.map(game => 
-                    <li key={game.id}>
+                    <li key={game.id} className="gameList-row">
+                        <Image 
+                            src={STORAGE_URL + game.cover}
+                            unoptimized
+                            className="gameCard-figure__cover"
+                            alt="Game Cover"
+                            width={100}
+                            height={300}
+                        />
 
+                        <h2 className="gameCard-title"> {game.title} </h2>
+                        <h2 className="gameCard-category"> {game.category} </h2>
                     </li>
                 )
             }
