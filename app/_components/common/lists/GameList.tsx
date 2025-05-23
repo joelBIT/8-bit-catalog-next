df3ce7847149b8401c03101da9e1d7b112de5c4a
joelBit <joel.rollny@gmail.com>
2025-05-23 12:21:52 +0200

    Added heading to favourite list

--- app/_components/common/lists/GameList.tsx
+++ app/_components/common/lists/GameList.tsx
@@ -10,6 +10,14 @@ export function GameList({ games }: { games: Game[] }): ReactElement {
 
     return (
         <ul id="gameList">
+            <section id="listHeading">
+                <h2 className="game-title"> Title </h2>
+                <h2 className="game-category"> Category </h2>
+                <h2 className="game-players"> Players </h2>
+                <h2 className="game-developer"> Developer </h2>
+                <h2 className="game-publisher"> Publisher </h2>
+            </section>
+
             {
                 games.map(game => <FavouriteListEntry game={game} key={game.id} />)
             }
