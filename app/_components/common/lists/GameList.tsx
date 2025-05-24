dd4fb7213384f603b5db1fddafff33e73b2116d8
joelBit <joel.rollny@gmail.com>
2025-05-24 14:37:45 +0200

    Added sections for game list headings

--- app/_components/common/lists/GameList.tsx
+++ app/_components/common/lists/GameList.tsx
@@ -7,18 +7,38 @@ import { GameListEntry } from "./GameListEntry";
 import "./GameList.css";
 
 /**
- * This component consists of a game list (in List View) and its heading.
+ * This component consists of a game list (in List View) and its heading. It is possible to sort the supplied
+ * list of games by clicking on each heading.
  */
 export function GameList({ games }: { games: Game[] }): ReactElement {
 
     return (
         <ul id="gameList">
             <section id="listHeading">
-                <h2 className="game-title"> Title </h2>
-                <h2 className="game-category"> Category </h2>
-                <h2 className="game-players"> Players </h2>
-                <h2 className="game-developer"> Developer </h2>
-                <h2 className="game-publisher"> Publisher </h2>
+                <section id="game-title">
+                    <span className="material-symbols-outlined"> keyboard_arrow_down </span>
+                    <h2 className="game-title"> Title </h2>
+                </section>
+                
+                <section id="game-category">
+                    <span className="material-symbols-outlined"> keyboard_arrow_down </span>
+                    <h2 className="game-category"> Category </h2>
+                </section>
+
+                <section id="game-players">
+                    <span className="material-symbols-outlined"> keyboard_arrow_down </span>
+                    <h2 className="game-players"> Players </h2>
+                </section>
+
+                <section id="game-developer">
+                    <span className="material-symbols-outlined"> keyboard_arrow_down </span>
+                    <h2 className="game-developer"> Developer </h2>
+                </section>                
+                
+                <section id="game-publisher">
+                    <span className="material-symbols-outlined"> keyboard_arrow_down </span>
+                    <h2 className="game-publisher"> Publisher </h2>
+                </section>
             </section>
 
             {
