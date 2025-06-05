992426632c4415dd2f0efda88b2685a004c33d49
joelBit <joel.rollny@gmail.com>
2025-06-05 16:37:50 +0200

    Temp spinner

---
+++ app/(site)/about/loading.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react";
+
+import "./loading.css";
+
+export default function Loading(): ReactElement {
+    return (
+        <main id="loadingPage">
+            <div className="loadingio-eclipse">
+                <div className="ldio-rpinwye8j0b">
+                    <div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
