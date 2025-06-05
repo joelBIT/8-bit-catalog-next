c8ec920018c27eca781379f6dc517c2e451a53da
joelBit <joel.rollny@gmail.com>
2025-06-05 07:01:35 +0200

    Added back link on forgot password page

--- app/(auth)/forgot/page.tsx
+++ app/(auth)/forgot/page.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import Link from "next/link";
 
 import "./page.css";
 
@@ -8,7 +9,18 @@ import "./page.css";
 export default function ForgotPage(): ReactElement {
     return (
         <main id="forgotPage">
+            <section id="forgotPage-modal">
+                <section id="forgotPage-modal-back">
+                    <Link href={"/"} className="forgotPage-link"> 
+                        <span className="material-symbols-outlined"> arrow_back </span> 
+                        <h2 className="forgotPage-link__text"> Back to catalog </h2> 
+                    </Link>
+                </section>
 
+                <section id="forgotPage-form">
+                    
+                </section>
+            </section>
         </main>
     );
 }
