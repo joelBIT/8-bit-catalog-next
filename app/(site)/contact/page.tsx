45e08e77d56ccdf6d80171a7ef33d4b8200bcb53
joelBit <joel.rollny@gmail.com>
2025-06-06 13:10:19 +0200

    Deleted Contact component

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -8,7 +8,22 @@ import "./page.css";
 export default function ContactPage(): ReactElement {
     return (
         <main id="contactPage">
-
+                
         </main>
     );
 }
+
+
+/**
+                    <h4 className="contact__info">
+                        <span className={`material-symbols-outlined mail`}> mail </span>
+                        <a href="mailto:joel.rollny@gmail.com" className="contact__link"> joel.rollny@gmail.com</a>
+                    </h4>
+                    <h4 className="contact__info">
+                        <span className={`material-symbols-outlined globe`}> globe </span> 
+                        <a href="https://www.joel-rollny.eu" target="_blank" className="contact__link"> www.joel-rollny.eu</a>
+                    </h4>
+                    <h4 className="contact__info">
+                        <span className={`material-symbols-outlined location`}> location_on </span> Karlstad, Sweden
+                    </h4>
+ */
