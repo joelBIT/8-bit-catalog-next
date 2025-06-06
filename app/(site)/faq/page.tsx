5f3d4f4c823c5aaa70760ad4a5795dcaa73b582e
joelBit <joel.rollny@gmail.com>
2025-06-06 16:43:42 +0200

    Created heading for FAQ page

--- app/(site)/faq/page.tsx
+++ app/(site)/faq/page.tsx
@@ -8,7 +8,10 @@ import "./page.css";
 export default function FaqPage(): ReactElement {
     return (
         <main id="faqPage">
-
+            <section className="title-section">
+                <h1 className="faqPage-title"> Frequently Asked Questions </h1>
+                <h2 className="title-text"> The most frequently asked questions are gathered here. If you do not find what you are looking for, let me know, and I will be happy to assist you. </h2>
+            </section>
         </main>
     );
 }
