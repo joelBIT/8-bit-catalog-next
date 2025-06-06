4851ebf3e26c00ba718191f6c54085e9edad7e09
joelBit <joel.rollny@gmail.com>
2025-06-06 14:27:24 +0200

    Added backgrounds to link cards

--- app/(site)/sites/page.tsx
+++ app/(site)/sites/page.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import Link from "next/link";
 
 import "./page.css";
 
@@ -8,17 +9,21 @@ import "./page.css";
 export default function SitesPage(): ReactElement {
     return (
         <main id="sitesPage">
-            
+            <Link id="nesDev" className="other-page-card" href="https://www.nesdev.org/" target="_blank">
+                <h1 className="card-title"> NesDev </h1>
+            </Link>
 
+            <Link id="nesCart" className="other-page-card" href="https://nescartdb.com/" target="_blank">
+                <h1 className="card-title"> NesCartDB </h1>
+            </Link>
+
+            <Link id="nesNinja" className="other-page-card" href="https://nesninja.com/game/nes/" target="_blank">
+                <h1 className="card-title"> NES Ninja </h1>
+            </Link>
+
+            <Link id="romDetectives" className="other-page-card" href="http://www.romdetectives.com/Wiki/index.php" target="_blank">
+                <h1 className="card-title"> ROM Detectives </h1>
+            </Link>
         </main>
     );
 }
-
-/**
- *     const OTHERLINKS = [
-        {url: "https://www.nesdev.org/", title: "NesDev"},
-        {url: "https://nesninja.com/game/nes/", title: "NES Ninja"},
-        {url: "https://nescartdb.com/", title: "NesCartDB"},
-        {url: "http://www.romdetectives.com/Wiki/index.php", title: "ROM Detectives"}
-    ];
- */
