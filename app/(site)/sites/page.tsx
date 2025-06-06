f0a1a18f7b5d8a4223903e96a4181b30a492f9b6
joelBit <joel.rollny@gmail.com>
2025-06-06 15:24:47 +0200

    Added temporary text to site cards

--- app/(site)/sites/page.tsx
+++ app/(site)/sites/page.tsx
@@ -17,17 +17,20 @@ export default function SitesPage(): ReactElement {
 
             <Link id="nesCart" className="other-page-card" href="https://nescartdb.com/" target="_blank">
                 <section className="background-wrapper" />
-                <h1 className="card-title"> NesCartDB </h1>
+                <h1 className="card-title"> NES Cart Database </h1>
+                <p className="card-description"> Dive into detailed information about NES carts and their hardware. </p>
             </Link>
 
             <Link id="nesNinja" className="other-page-card" href="https://nesninja.com/game/nes/" target="_blank">
                 <section className="background-wrapper" />
                 <h1 className="card-title"> NES Ninja </h1>
+                <p className="card-description"> Play all the classic 8-bit Nintendo video games online for free. Download retro NES game on NESNinja.com. </p>
             </Link>
 
             <Link id="romDetectives" className="other-page-card" href="http://www.romdetectives.com/Wiki/index.php" target="_blank">
                 <section className="background-wrapper" />
                 <h1 className="card-title"> ROM Detectives </h1>
+                <p className="card-description"> Your Wiki guide to video game hacking. Their goal is to reverse engineer every video game ever made to understand how they were programmed. </p>
             </Link>
         </main>
     );
