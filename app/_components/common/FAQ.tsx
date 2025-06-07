a13438bac032cf9e4501e71d4e486726a438cdb7
joelBit <joel.rollny@gmail.com>
2025-06-07 17:36:50 +0200

    Removed icon from FAQ questions

--- app/_components/common/FAQ.tsx
+++ app/_components/common/FAQ.tsx
@@ -14,10 +14,7 @@ export function FAQ({ faq }: { faq: FAQ }): ReactElement {
     return (
         <section className="faq">
              <section className="faq-card">
-                <section className="faq-card-text">
-                    <span className="material-symbols-outlined"> help </span>
-                    <h2 className="faq-card__question" onClick={() => setIsShowing(!isShowing)}> { faq.question } </h2> 
-                </section>
+                <h2 className="faq-card__question" onClick={() => setIsShowing(!isShowing)}> { faq.question } </h2> 
 
                 <div className={isShowing ? "faq-open" : "faq-closed"} onClick={() => setIsShowing(!isShowing)} />
             </section>
