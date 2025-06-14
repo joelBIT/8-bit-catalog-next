c899943adc4935dad29f4e33cc43cd738b60d313
joelBit <joel.rollny@gmail.com>
2025-06-14 14:30:05 +0200

    Changed site cards appearance

--- app/(site)/sites/page.tsx
+++ app/(site)/sites/page.tsx
@@ -9,29 +9,46 @@ import "./page.css";
 export default function SitesPage(): ReactElement {
     return (
         <main id="sitesPage">
-            <Link id="nesDev" className="other-page-card" href="https://www.nesdev.org/" target="_blank">
-                <section className="background-wrapper" />
-                <h1 className="card-title"> NesDev </h1>
-                <p className="card-description"> A community of homebrew game developers and hardware researchers for the Nintendo Entertainment System (NES) and other retro consoles. </p>
-            </Link>
+            <div className="darken-image-top" />
+            <h1 className="sites-title playfair-display-regular"> Other 8-bit resources </h1>
 
-            <Link id="nesCart" className="other-page-card" href="https://nescartdb.com/" target="_blank">
-                <section className="background-wrapper" />
-                <h1 className="card-title"> NES Cart Database </h1>
-                <p className="card-description"> Dive into detailed information about NES carts and their hardware. </p>
-            </Link>
+            <section id="other-site-cards">
+                <Link id="nesDev" className="other-page-card" href="https://www.nesdev.org/" target="_blank">
+                    <section className="other-information">
+                        <h2 className="site-title"> NesDev </h2>
+                        <div className="site-description">
+                            A community of homebrew game developers and hardware researchers for the Nintendo Entertainment System (NES) and other retro consoles.
+                        </div>
+                    </section>
+                </Link>
 
-            <Link id="nesNinja" className="other-page-card" href="https://nesninja.com/game/nes/" target="_blank">
-                <section className="background-wrapper" />
-                <h1 className="card-title"> NES Ninja </h1>
-                <p className="card-description"> Play all the classic 8-bit Nintendo video games online for free. Download retro NES game on NESNinja.com. </p>
-            </Link>
+                <Link id="nesCart" className="other-page-card" href="https://nescartdb.com/" target="_blank">
+                    <section className="other-information">
+                        <h2 className="site-title"> NES Cart Database </h2>
+                        <div className="site-description">
+                            Dive into detailed information about NES carts and their hardware.
+                        </div>
+                    </section>
+                </Link>
 
-            <Link id="romDetectives" className="other-page-card" href="http://www.romdetectives.com/Wiki/index.php" target="_blank">
-                <section className="background-wrapper" />
-                <h1 className="card-title"> ROM Detectives </h1>
-                <p className="card-description"> Your Wiki guide to video game hacking. Their goal is to reverse engineer every video game ever made to understand how they were programmed. </p>
-            </Link>
+                <Link id="nesNinja" className="other-page-card" href="https://nesninja.com/game/nes/" target="_blank">
+                    <section className="other-information">
+                        <h2 className="site-title"> NES Ninja </h2>
+                        <div className="site-description">
+                            Play all the classic 8-bit Nintendo video games online for free. Download retro NES game on NESNinja.com.
+                        </div>
+                    </section>
+                </Link>
+
+                <Link id="romDetectives" className="other-page-card" href="http://www.romdetectives.com/Wiki/index.php" target="_blank">
+                    <section className="other-information">
+                        <h2 className="site-title"> ROM Detectives </h2>
+                        <div className="site-description">
+                            Your Wiki guide to video game hacking. Their goal is to reverse engineer every video game ever made to understand how they were programmed.
+                        </div>
+                    </section>
+                </Link>
+            </section>
         </main>
     );
 }
