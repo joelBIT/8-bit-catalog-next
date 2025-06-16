cf39a0f7460b1b9790a1fe1ef4347d38ffb79d90
joelBit <joel.rollny@gmail.com>
2025-06-16 11:32:03 +0200

    Changed gameButton in EditProfileForm

--- app/_components/account/EditProfileForm.tsx
+++ app/_components/account/EditProfileForm.tsx
@@ -38,7 +38,7 @@ export function EditProfileForm(): ReactElement {
                     </h2>
                 </section>
 
-                <button className="gameButton" type="submit"> Save </button>
+                <span className="button__link" > Save </span>
             </form>
 
             { 
