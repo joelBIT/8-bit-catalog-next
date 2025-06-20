027d0ed1d5b0932fc26fb94733d2833a2e2b7dce
joelBit <joel.rollny@gmail.com>
2025-06-20 11:20:52 +0200

    Created a page for updating an account password that has been reset

--- app/_actions/auth.ts
+++ app/_actions/auth.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { Resend } from "resend";
 import { v4 as uuidv4 } from 'uuid';
-import { emailExists, createAccount, getAccountByUserId, getUserByEmail, registerUser, updateUserPassword, updateUserInformationById } from "@/app/_db/db";
+import { emailExists, createAccount, getAccountByUserId, getUserByEmail, registerUser, updateUserPassword, updateUserInformationById, isCurrentPassword } from "@/app/_db/db";
 import { hashPassword, verifyPasswordHash } from "@/app/_session/password";
 import { createSession, generateRandomSessionToken } from "@/app/_session/session";
 import ResetPasswordEmail from "../_components/email/ResetPasswordEmail";
@@ -149,6 +149,52 @@ async function sendPasswordResetMail(email: string, password: string): Promise<v
         from: '8bit <onboarding@joel-rollny.eu>',
         to: email,
         subject: 'Password Reset',
-        react: ResetPasswordEmail(password),
+        react: ResetPasswordEmail(password, email),
     });
+}
+
+/**
+ * This function is invoked when a user changes password from the reset password to a new password for an account.
+ */
+export async function changeAccountPassword(_prevState: ActionState, formData: FormData): Promise<ActionState> {
+    const email = formData.get('email') as string;
+    const oldPassword = formData.get('oldPassword') as string;
+    const newPassword = formData.get('password') as string;
+    const passwordRepeat = formData.get('passwordRepeat') as string;
+
+    console.log('email ' + email);
+
+    try {
+        const exists = await emailExists(email);
+        if (!exists) {
+            return { message: 'No such email', success: false };
+        }
+
+        const oldPasswordHash = await hashPassword(oldPassword);
+        if (!isCurrentPassword(email, oldPasswordHash)) {
+            return { message: 'Old password is not correct', success: false };
+        }
+        
+        if (newPassword !== passwordRepeat) {
+            return { message: 'Passwords must be equal', success: false };
+        }
+
+        if (newPassword.length < 8) {
+            return { message: 'Password must be at least 8 characters', success: false };
+        }
+
+        if (!/\d/.test(newPassword)) {
+            return { message: 'Password must contain at least 1 number', success: false };
+        }
+
+        const passwordHash = await hashPassword(newPassword);
+        await updateUserPassword(email, passwordHash);
+        
+        return { message: 'Password updated', success: true};
+    } catch (error) {
+        if (error instanceof Error) {
+            return { message: error.message, success: false };
+        }
+        return { message: 'Could not change password', success: false };
+    }
 }
--- app/_components/email/ResetPasswordEmail.tsx
+++ app/_components/email/ResetPasswordEmail.tsx
@@ -1,9 +1,10 @@
-import { Section, Row, Text } from "@react-email/components";
+import { Section, Row, Text, Link } from "@react-email/components";
+import { URL_RESET_PAGE } from "@/app/_utils/utils";
 
 /**
  * This email is sent to the email address supplied when requesting a new password.
  */
-export default function ResetPasswordEmail(password: string) {
+export default function ResetPasswordEmail(password: string, email: string) {
     return (
         <Section>
             <Section>
@@ -14,7 +15,27 @@ export default function ResetPasswordEmail(password: string) {
                 </Row>
                 <Row>
                     <Text style={{ fontSize: "1.3rem"}}>
-                        Hi! You requested a new password which is: {password}
+                        Hello, 
+                        The 8-bit Catalog has received a request to reset the password for the 8-bit Catalog account 
+                        associated with {email}. The new password of the account is {password}.
+                        You can change this password by clicking on the link below or by signing in to your account 
+                        and update the password in your settings. 
+                    </Text>
+                </Row>
+                <Row>
+                    <Text style={{ fontSize: "1.3rem"}}>
+                        Please click on the following link to change your password: <Link href={`${process.env.DOMAIN_URL}${URL_RESET_PAGE}?email=${email}`}> Change Password </Link>
+                    </Text>
+                </Row>
+                <Row>
+                    <Text style={{ fontSize: "1.3rem"}}>
+                        If you did not request a new password, please let me know immediately by sending an email to joel.rollny@gmail.com
+                    </Text>
+                </Row>
+                <Row>
+                    <Text style={{ fontSize: "1.3rem"}}>
+                        You can find answers to many questions and get in touch with me at <Link href={`${process.env.DOMAIN_URL}/contact`}> 8bit-catalog.joel-rollny.eu/contact</Link>. 
+                        I am here to help you at any step along the way.
                     </Text>
                 </Row>
             </Section>
