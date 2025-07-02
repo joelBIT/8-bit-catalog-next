eb2c6e5c7afa4ad8c9205cf6a4ab9a33f07f0ace
joelBit <joel.rollny@gmail.com>
2025-07-02 13:21:05 +0200

    Use country object value

--- app/_components/common/SelectCountry.tsx
+++ app/_components/common/SelectCountry.tsx
@@ -43,7 +43,7 @@ export function SelectCountry({ selected }: { selected: string }): ReactElement 
             <select 
                 id="selectCountry" 
                 name="country" 
-                value={selectedCountry} 
+                value={selectedCountry.value}
                 onChange={selectCountry} 
                 className={`${arima.className} input-field`}
             >
