cf807a44d67a8515105148f3ce9a6df67b153828
joelBit <joel.rollny@gmail.com>
2025-07-06 09:27:07 +0200

    Use UserList instead

--- app/_components/common/lists/UserList.tsx
+++ app/_components/common/lists/UserList.tsx
@@ -1,18 +1,31 @@
 'use client';
 
-import {ReactElement, useState} from "react";
+import {ReactElement, useEffect, useState} from "react";
 import {useSearchParams} from "next/navigation";
-import { User } from "@/app/_types/types";
+import {UserListEntry} from "@/app/_components/common";
+import {User} from "@/app/_types/types";
 
 import "./UserList.css";
 
-export function UserList(): ReactElement {
+export function UserList({ users }: { users: User[] }): ReactElement {
     const searchParams = useSearchParams();
     const params = new URLSearchParams(searchParams);
     const showModal = !!params.get('show');
     const [ openModal, setOpenModal ] = useState<boolean>(showModal);
     const [ selectedUser, setSelectedUser] = useState<User>({} as User);
-    const HEADINGS = ["Name", "Email", "Role", "Account", "Joined"];
+    const [ currentUsers, setCurrentUsers ] = useState<User[]>([]);
+    const USER_NAME = "user-name";
+    const USER_EMAIL = "user-email";
+    const USER_ROLE = "user-role";
+    const USER_ACCOUNT = "user-account";
+    const USER_JOINED = "user-joined";
+    const HEADING_CLASSES = [USER_EMAIL, USER_NAME, USER_ROLE, USER_ACCOUNT, USER_JOINED];
+    const HEADINGS = ["Email", "Name", "Role", "Account", "Joined"];
+
+    useEffect(() => {
+        setCurrentUsers(users);
+        setOpenModal(showModal);        // Close modal if navigating back from modal to page
+    })
 
     function openUserModal(user: User): void {
         closeUserModal();       // Handles when back button on mobile phone is used, makes sure the url is really updated before opening modal
@@ -34,7 +47,17 @@ export function UserList(): ReactElement {
 
     return (
         <ul id="userList">
+            <section id="listHeading">
+                {
+                    HEADING_CLASSES.map((heading, index) =>
+                        <h2 className={heading} key={heading}> {HEADINGS[index]} </h2>
+                    )
+                }
+            </section>
 
+            { openModal && Object.keys(selectedUser).length > 0 ? <UserModal game={selectedUser} close={() => closeUserModal()} /> : <></> }
+
+            { currentUsers.length > 0 ? currentUsers.map(user => <UserListEntry user={user} key={user.id} click={openUserModal} />) : <></> }
         </ul>
     );
 }
--- app/_components/common/modals/UserModal.tsx
+++ app/_components/common/modals/UserModal.tsx
@@ -1,8 +1,9 @@
 import {ReactElement} from "react";
+import {User} from "@/app/_types/types";
 
 import "./UserModal.css";
 
-export function UserModal(): ReactElement {
+export function UserModal({ user, close }: { user: User, close: () => void }): ReactElement {
     return (
         <dialog id="userModal">
 
