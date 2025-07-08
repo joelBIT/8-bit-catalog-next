80e797e4b609459e4a8adc51108d473d14c2ad30
joelBit <joel.rollny@gmail.com>
2025-07-08 09:35:16 +0200

    Changed logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,18 +11,10 @@ export default async function Home(): Promise<ReactElement> {
 
     return (
         <main id="landingPage">
-            <section id="image-wrapper">
-                <svg viewBox="0 0 30 3">
-                    <path id="landing-title" fill="none" d="M 0 5 Q 15 -5 30 5" pathLength="2" />
-                    <text id="landing-text" fill="#f0cca4" className={`${irishGrover.className}`} fontSize="2" dominantBaseline="hanging" textAnchor="middle">
-                        <textPath href="#landing-title" startOffset="1">
-                            The 8-bit Catalog
-                        </textPath>
-                    </text>
-                </svg>
 
                 <Logo />
-            </section>
+
+
         </main>
     );
 }
