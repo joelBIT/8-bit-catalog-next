49dc199f1ce13becad2b6a0493402348247a439f
joelBit <joel.rollny@gmail.com>
2025-07-08 10:03:23 +0200

    Added text and link to register page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
+import Link from "next/link";
 import { Logo } from "../_components/header";
-import { irishGrover } from "../_fonts/fonts";
+import {isAuthenticated} from "@/app/_session/utils";
 
 import "./page.css";
 
@@ -11,10 +12,14 @@ export default async function Home(): Promise<ReactElement> {
 
     return (
         <main id="landingPage">
-
+            <section id="image-wrapper">
                 <Logo />
+            </section>
 
-
+            <section id="landing-register">
+                <h2 className="landing-text"> Become a member to connect with other 8-bit enthusiasts </h2>
+                {await isAuthenticated() ? <></> : <Link href="/register" id="registerButton" className="authButton"> REGISTER </Link> }
+            </section>
         </main>
     );
 }
