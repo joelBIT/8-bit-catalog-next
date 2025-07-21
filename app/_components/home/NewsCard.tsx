f726ba1f4576f3cbc2cdf5563f05fb6340e2b236
joelBit <joel.rollny@gmail.com>
2025-07-21 17:57:30 +0200

    Use toString for date object

--- app/_components/home/NewsCard.tsx
+++ app/_components/home/NewsCard.tsx
@@ -5,7 +5,7 @@ import "./NewsCard.css";
 export function NewsCard({ text, date, heading }: { text: string, date: Date, heading: string }): ReactElement {
     return (
         <section className="news-card">
-            <h2 className="news-date"> {date} </h2>
+            <h2 className="news-date"> {date.toString()} </h2>
             <h2 className="news-heading"> {heading} </h2>
             <h2 className="news-text"> {text} </h2>
         </section>
