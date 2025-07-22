64c2359d6f78dcfb8d37b140434352b3f439a326
joelBit <joel.rollny@gmail.com>
2025-07-22 10:05:36 +0200

    Enabled registering email for newsletter subscription

---
+++ app/_actions/newsletter.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { ActionState } from "@/app/_types/types";
+import { subscribeNewsletter } from "@/app/_db/db";
+
+/**
+ * Creates a newsletter subscription for the supplied email. It is not required to have an account. A person only needs to enter an email
+ * address and click on the 'subscribe' button.
+ */
+export async function createNewsletterSubscription(_prevState: ActionState, formData: FormData): Promise<ActionState> {
+    const email = formData.get('email') as string;
+    if (!email) {
+        return { message: 'An email address is required', success: false };
+    }
+
+    try {
+        await subscribeNewsletter(email);
+
+        return { message: `${email} has now subscribed`, success: true };
+    } catch (error) {
+        console.log(error);
+        if (error instanceof Error) {
+            return { message: error.message, success: false };
+        }
+
+        return { message: 'Subscription could not be created', success: false };
+    }
+}
--- app/_components/home/SubscriptionBox.tsx
+++ app/_components/home/SubscriptionBox.tsx
@@ -1,20 +1,39 @@
 'use client';
 
-import { ReactElement } from "react";
+import { ReactElement, useActionState, useEffect, useState } from "react";
+import { createNewsletterSubscription } from "@/app/_actions/newsletter";
 import { arima } from "@/app/_fonts/fonts";
 
 import "./SubscriptionBox.css";
 
 export function SubscriptionBox(): ReactElement {
-    return (
-        <section id="newsletter">
-            <img src="/metroidscreen.webp" id="newsletterBackground" alt="Metroid newsletter background" />
-            <h2 className="subscription-text"> Subscribe to receive the newsletter.</h2>
+    const [ showMessage, setShowMessage ] = useState<boolean>(false);
+    const [ state, formAction ] = useActionState(createNewsletterSubscription, { message: '', success: false });
 
-            <form id="newsletterSignup">
-                <input id="subscribeEmail" type="email" placeholder="Enter Email" required={true} />
-                <button id="subscribeButton" type="submit" className={`button__link ${arima.className}`}> Subscribe </button>
-            </form>
-        </section>
+    useEffect(() => {
+        if (state?.message && !showMessage) {       // Show message for a fixed amount of time
+            setShowMessage(true);
+        }
+    }, [state]);
+
+    return (
+        <>
+            <section id="newsletter">
+                <img src="/metroidscreen.webp" id="newsletterBackground" alt="Metroid newsletter background" />
+                <h2 className="subscription-text"> Subscribe to receive the newsletter.</h2>
+
+                <form id="newsletterSignup" action={formAction}>
+                    <input id="subscribeEmail" type="email" name="email" placeholder="Enter Email" required={true} />
+                    <button id="subscribeButton" type="submit" className={`button__link ${arima.className}`}> Subscribe </button>
+                </form>
+            </section>
+
+            {
+                showMessage ?
+                    <h2 className={state?.success ? "message-success" : "message-failure"}>
+                        {state?.message}
+                    </h2> : <></>
+            }
+        </>
     );
 }
