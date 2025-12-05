1c0b07525f994466f7d061e67538dbddbb9e4dbd
joelBit <joel.rollny@gmail.com>
2025-12-05 15:51:38 +0100

    Created RangeSlider for use when search result is greater than 80

---
+++ app/_components/search/RangeSlider.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState, type ReactElement } from "react";
+
+import "./RangeSlider.css";
+
+/**
+ * Slider for changing how many games are visible in the search result. 
+ * When mouse state is "up" the parent is called with that chosen value (avoids parent rerendering all the time when sliding).
+ */
+export function RangeSlider({min, max, setSliderValue}: {min: number, max: number, setSliderValue: (value: number) => void}): ReactElement {
+    const [value, setValue] = useState<number>(min);
+
+    return (
+        <section className="rangeSlider">
+            <input 
+                type="range" 
+                min={min} 
+                step="1" 
+                max={max} 
+                value={value >= max ? max : value} 
+                className="slider" 
+                onInput={(e) => setValue(parseInt((e.target as HTMLInputElement).value))} 
+                onMouseUp={(e) => setSliderValue(parseInt((e.target as HTMLInputElement).value))} 
+            />
+
+            <h2 className="slider-text"> Showing {value >= max ? max : value} / {max} games</h2>
+        </section>
+    )
+}
--- app/_components/search/index.ts
+++ app/_components/search/index.ts
@@ -1,5 +1,6 @@
 export * from "./CategoryFilter";
 export * from "./DeveloperFilter";
 export * from "./PublisherFilter";
+export * from "./RangeSlider";
 export * from "./Search";
 export * from "./SearchButton";
