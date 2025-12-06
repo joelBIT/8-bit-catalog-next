b29ae23ec9876a50b4993738b13c76fdb4c36946
joelBit <joel.rollny@gmail.com>
2025-12-06 15:41:15 +0100

    Moved texts to array

--- app/_components/home/LandingSelection.tsx
+++ app/_components/home/LandingSelection.tsx
@@ -7,58 +7,44 @@ import { News } from "@/app/_types/types";
 import "./LandingSelection.css";
 
 export function LandingSelection({news}: {news: News[]}): ReactElement {
-    const options = ["news", "favourites"];
-    const [active, setActive] = useState<string>(options[0]);
+    const [active, setActive] = useState<number>(0);
+
+    const text = [
+        "I publish news when something interesting occurs in this hobby project. An email is sent to those who have an active newsletter subscription when news is published.",
+        "You can label games of your choosing as favourites. Such games are easily accessed by clicking on the Favourites link. Favourites are persisted permanently if you have signed in to your account when labeling them."
+    ]
     
     return (
         <section id="landingSelection">
             <section id="landing-selection-top">
                 <ul id="landing-selection-list">
                     <li 
-                        className={active === options[0] ? "landing-selection-list__item active" : "landing-selection-list__item"} 
-                        onClick={() => setActive(options[0])}
+                        className={active === 0 ? "landing-selection-list__item active" : "landing-selection-list__item"} 
+                        onClick={() => setActive(0)}
                     > 
-                        {active === options[0] ? <h3 className="item-arrow"> &#10148; </h3> : <></>}
+                        {active === 0 ? <h3 className="item-arrow"> &#10148; </h3> : <></>}
                         <h3 className="item-title"> News </h3>
                     </li>
 
                     <li 
-                        className={active === options[1] ? "landing-selection-list__item active" : "landing-selection-list__item"} 
-                        onClick={() => setActive(options[1])}
+                        className={active === 1 ? "landing-selection-list__item active" : "landing-selection-list__item"} 
+                        onClick={() => setActive(1)}
                     > 
-                        {active === options[1] ? <h3 className="item-arrow"> &#10148; </h3> : <></>}
+                        {active === 1 ? <h3 className="item-arrow"> &#10148; </h3> : <></>}
                         <h3 className="item-title"> Choose Favourites </h3>
                     </li>
                 </ul>
                 
-                {
-                    active === "favourites" ? 
-                            <p id="landing-selection-text"> 
-                                You can label games of your choosing as favourites. Such games are easily accessed by clicking on the Favourites link.
-                                Favourites are persisted permanently if you have signed in to your account when labeling them.
-                            </p>
-                            :
-                            <p id="landing-selection-text"> 
-                                I publish news when something interesting occurs in this hobby project.
-                                An email is sent to those who have an active newsletter subscription when news is published.
-                            </p>
-                }
+                <p id="landing-selection-text"> 
+                    { text[active] }
+                </p>
             </section>
 
-            {
-                active === "favourites" ?
-                    <section id="favouriteCarousel">
-                        {
-                            news.map(news => <NewsCard key={news.text} news={news} />)
-                        }
-                    </section>
-                    :
-                    <section id="newsCards">
-                        {
-                            news.map(news => <NewsCard key={news.text} news={news} />)
-                        }
-                    </section>
-            }
+            <section id="newsCards">
+                {
+                    news.map(news => <NewsCard key={news.text} news={news} />)
+                }
+            </section>
         </section>
     )
 }
