745b0e266557b9d16aab3f391fef1d66e6777f6b
joelBit <joel.rollny@gmail.com>
2025-12-08 12:27:23 +0100

    Added register link on landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,11 @@
 'use server';
 
 import { type ReactElement } from "react";
+import Link from "next/link";
 import { Logo } from "../_components/header";
 import { LandingCard, LandingSelection, TitleSearch } from "@/app/_components/home";
 import { getAllNews, getAllTitles } from "@/app/_db/db";
+import { URL_REGISTER_PAGE } from "../_utils/utils";
 
 import "./page.css";
 
@@ -27,6 +29,15 @@ export default async function Home(): Promise<ReactElement> {
                 <section id="searchGameArea">
                     <TitleSearch titles={await getAllTitles()} />
                 </section>
+            </section>
+
+            <section id="joinCatalog">
+                <h2 className="joinCatalog-title"> Join the 8-bit family </h2>
+                <p className="joinCatalog-text"> 
+                    Become part of the catalog and unlock services including real-time interaction with other members, exclusive offers, info about retro events, and more.
+                </p>
+                
+                <Link href={URL_REGISTER_PAGE} id="signup-button"> Sign up now </Link>
             </section>
 
             <section id="secondSection">
