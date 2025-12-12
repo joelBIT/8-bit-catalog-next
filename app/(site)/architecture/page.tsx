2b9a6ff27097b8ad06e26cf967be10dd1081ed21
joelBit <joel.rollny@gmail.com>
2025-12-12 10:17:22 +0100

    Added background and some styling to architecture page

--- app/(site)/architecture/page.tsx
+++ app/(site)/architecture/page.tsx
@@ -5,32 +5,31 @@ import "./page.css";
 export default function ArchitecturePage(): ReactElement {
     return (
         <main id="architecturePage">
+            <img src="/donkeykong.avif" className="background-image" />
+
             <section id="architecture-top">
                 <article className="architecture-top-left">
-                    <div className="nintendo-presentation">
+                    <section className="nintendo-presentation">
                         <h2 className="nintendo-presentation__heading"> Regarded as one of the most influential gaming consoles. </h2>
                         <p className="nintendo-presentation__text">
                             The NES pioneered a now-standard business model of licensing third-party developers to produce and distribute games.
-                            It was released in Japan on July 15, 1983, as the Family Computer (Famicom). Today, the NES has a thriving community.
+                            It was released in Japan on July 15, 1983, as the Family Computer (Famicom). Today, the NES is the 14th best-selling console 
+                            of all time and has a thriving community.
                         </p>
-                    </div>
+                    </section>
 
                     <section className="nintendo-info">
-                        <div className="nintendo-info-hardware"> 
-                            <ul className="nintendo-info-hardware__list">
-                                <li className="nintendo-info-hardware__list-item">
-                                    <h4>CPU</h4> <p>8-bit, 1.66 Mhz</p>
-                                </li>
-                                <li className="nintendo-info-hardware__list-item">
-                                    <h4>Memory</h4>	<p>2 Kb RAM - Video RAM 2 Kb</p>
-                                </li>
-                                <li className="nintendo-info-hardware__list-item">
-                                    <h4>Display</h4> <p>256 x 240 pixel maximum resolution, 52 colours, 8 x 16 pixel maximum sprite size, 64 sprites on-screen</p>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="nintendo-info-sold"> The 14th best-selling console of all time! </div>
+                        <ul className="nintendo-info-hardware__list">
+                            <li className="nintendo-info-hardware__list-item">
+                                <h4>CPU</h4> <p>8-bit, 1.66 Mhz</p>
+                            </li>
+                            <li className="nintendo-info-hardware__list-item">
+                                <h4>Memory</h4>	<p>2 Kb RAM - Video RAM 2 Kb</p>
+                            </li>
+                            <li className="nintendo-info-hardware__list-item">
+                                <h4>Display</h4> <p>256 x 240 pixel maximum resolution, 52 colours, 8 x 16 pixel maximum sprite size, 64 sprites on-screen</p>
+                            </li>
+                        </ul>
                     </section>
                 </article>
 
@@ -60,13 +59,17 @@ export default function ArchitecturePage(): ReactElement {
                     Read about the NES console architecture. The general architecture is covered in the following articles.
                 </h3>
 
-                <section className="articleCard">
-                    <img src="/famicom.webp" alt="PPU article" />
-                    <div className="article-tag"> Graphics </div>
-                    <h2>PPU</h2>
-                    <p>Some text about PPU</p>
+                <section id="articles">
+                    <section className="articleCard">
+                        <img src="/famicom.webp" alt="PPU article" />
+                        <div className="article-tag"> Graphics </div>
+                        <h2 className="article-title">PPU</h2>
+                        <p className="article-text">Some text about PPU</p>
+                    </section>
                 </section>
             </section>
+
+            <div className="darken-image-bottom" />
         </main>
     )
 }
