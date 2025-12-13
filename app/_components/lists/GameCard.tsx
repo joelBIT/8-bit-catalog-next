9d1866f834793c524289db12a6776009f3bfc3a7
joelBit <joel.rollny@gmail.com>
2025-12-13 12:48:07 +0100

    Added favourite sign on upper left part of cover image

--- app/_components/lists/GameCard.tsx
+++ app/_components/lists/GameCard.tsx
@@ -2,7 +2,7 @@
 
 import { ReactElement, useState } from "react";
 import Image from 'next/image';
-import { useGame } from "@/app/_hooks";
+import { useFavourites, useGame } from "@/app/_hooks";
 import { FavouriteButton } from "@/app/_components/favourites";
 import { Game } from "@/app/_types/types";
 
@@ -12,21 +12,32 @@ import "./GameCard.css";
  * Represents a card containing information about a game. The card is clickable so that
  * a user can navigate to the game's detail page if interested. This game card is used in Grid Views.
  */
-export function GameCard({ game, click }: { game: Game, click: (game: Game) => void }): ReactElement {
+export function GameCard({ game, openModal }: { game: Game, openModal: (game: Game) => void }): ReactElement {
     const [isFadingOut, setIsFadingOut] = useState<boolean>(false);
     const [removeCard, setRemoveCard] = useState<boolean>(false);
     const { setSelectedGame } = useGame();
+    const { isFavourite } = useFavourites();
+    const favourite = isFavourite(game.id);
     const STORAGE_URL = process.env.NEXT_PUBLIC_COVER;
 
     function showGame(): void {
         setSelectedGame(game);
-        click(game);
+        openModal(game);
     }
     
     return (
         <section className={isFadingOut ? "is-fading" : ""} onClick={showGame}>
             <section className={removeCard ? "hidden" : "gameCard"}>
                 <figure className="gameCard-figure">
+                    {
+                        favourite ? 
+                            <section className="game-favourite">
+                                <img src="/isFavourite.png" /> 
+                                <h3 className="game-favourite__text"> A favourite </h3> 
+                            </section> 
+                        : <></>
+                    }
+
                     <Image 
                         src={STORAGE_URL + game.cover}
                         unoptimized
@@ -39,12 +50,6 @@ export function GameCard({ game, click }: { game: Game, click: (game: Game) => v
                     <section className="gameCard-favourite">
                         <FavouriteButton game={game} setFading={setIsFadingOut} removeCard={setRemoveCard} />
                     </section>
-
-                    <div className="darken-image-bottom" />
-
-                    <article id="gameCardTitle">
-                        {game.title}
-                    </article>
                 </figure>
             </section>
         </section>
