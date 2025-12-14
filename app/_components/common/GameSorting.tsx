4cb5d24ffa9868af7c366a417fa740697485f1d7
joelBit <joel.rollny@gmail.com>
2025-12-14 09:43:19 +0100

    Moved sorting to GameSorting component

---
+++ app/_components/common/GameSorting.tsx
@@ -0,0 +1,58 @@
+import { ReactElement } from "react";
+import { Game } from "@/app/_types/types";
+
+import "./GameSorting.css";
+
+export function GameSorting({games, setSortedGames}: {games: Game[], setSortedGames: (games: Game[]) => void}): ReactElement {
+
+        /**
+     * Sort games according to selected option.
+     */
+    function sortGames(sort: string): void {
+        const gamesToSort = [...games];
+
+        if (sort === "titleAsc") {
+            const sorted = gamesToSort.sort((a, b) => a.title.localeCompare(b.title));
+            setSortedGames([...sorted]);
+        } else if (sort === "titleDes") {
+            const sorted = gamesToSort.sort((a, b) => b.title.localeCompare(a.title));
+            setSortedGames([...sorted]);
+        } else if (sort === "playersAsc") {
+            const sorted = gamesToSort.sort((a, b) => a.players - b.players);
+            setSortedGames([...sorted]);
+        } else if (sort === "playersDes") {
+            const sorted = gamesToSort.sort((a, b) => b.players - a.players);
+            setSortedGames([...sorted]);
+        } else if (sort === "publisherAsc") {
+            const sorted = gamesToSort.sort((a, b) => a.publisher.localeCompare(b.publisher));
+            setSortedGames([...sorted]);
+        } else if (sort === "publisherDes") {
+            const sorted = gamesToSort.sort((a, b) => b.publisher.localeCompare(a.publisher));
+            setSortedGames([...sorted]);
+        } else if (sort === "developerAsc") {
+            const sorted = gamesToSort.sort((a, b) => a.developer.localeCompare(b.developer));
+            setSortedGames([...sorted]);
+        } else if (sort === "developerDes") {
+            const sorted = gamesToSort.sort((a, b) => b.developer.localeCompare(a.developer));
+            setSortedGames([...sorted]);
+        }
+    }
+
+    return (
+        <section id="games-sort">
+            <label id="games-sort-label" htmlFor="games-sort-select"> Sort by: </label> 
+            <select id="games-sort-select" name="games-sort-select" onChange={e => sortGames(e.target.value)}>
+                <optgroup className="games-sort-select__options">
+                    <option value="titleAsc" defaultChecked> Title Ascending </option>
+                    <option value="titleDes"> Title Descending </option>
+                    <option value="playersAsc"> Players Ascending </option>
+                    <option value="playersDes"> Players Descending </option>
+                    <option value="publisherAsc"> Publisher Ascending </option>
+                    <option value="publisherDes"> Publisher Descending </option>
+                    <option value="developerAsc"> Developer Ascending </option>
+                    <option value="developerDes"> Developer Descending </option>
+                </optgroup>
+            </select>
+        </section>
+    )
+}
--- app/_components/search/Search.tsx
+++ app/_components/search/Search.tsx
@@ -4,7 +4,7 @@ import { ReactElement, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useGame, useGames } from "@/app/_hooks";
 import { Game } from "@/app/_types/types";
-import { ScrollTopButton } from "../common";
+import { GameSorting, ScrollTopButton } from "../common";
 import { RangeSlider } from ".";
 import { GameGrid, GameList, SlidingToggle } from "../lists";
 
@@ -23,7 +23,6 @@ export function Search(): ReactElement {
     const developer = params.get('developer') as string;
     const publisher = params.get('publisher') as string;
     const [searchResult, setSearchResult] = useState<Game[]>([]);
-    const [sortedGames, setSortedGames] = useState<Game[]>(searchResult);
     const [showHeading, setShowHeading] = useState<boolean>(false);
     const [totalCount, setTotalCount] = useState<number>();
     const [numberGamesShowing, setNumberGamesShowing] = useState<number>(50);           // Minimum 50 games visible
@@ -42,42 +41,8 @@ export function Search(): ReactElement {
     async function search(): Promise<void> {
         const filteredGames = getFilteredGames({title, category, developer, publisher});
         setSearchResult(filteredGames);
-        setSortedGames(filteredGames);
         setTotalCount(filteredGames.length);
         setShowHeading(true);           // Set to true after first search is executed
-    }
-
-     /**
-     * Sort games according to selected option.
-     */
-    function sortGames(sort: string): void {
-        const gamesToSort = [...sortedGames];
-
-        if (sort === "titleAsc") {
-            const sorted = gamesToSort.sort((a, b) => a.title.localeCompare(b.title));
-            setSortedGames([...sorted]);
-        } else if (sort === "titleDes") {
-            const sorted = gamesToSort.sort((a, b) => b.title.localeCompare(a.title));
-            setSortedGames([...sorted]);
-        } else if (sort === "playersAsc") {
-            const sorted = gamesToSort.sort((a, b) => a.players - b.players);
-            setSortedGames([...sorted]);
-        } else if (sort === "playersDes") {
-            const sorted = gamesToSort.sort((a, b) => b.players - a.players);
-            setSortedGames([...sorted]);
-        } else if (sort === "publisherAsc") {
-            const sorted = gamesToSort.sort((a, b) => a.publisher.localeCompare(b.publisher));
-            setSortedGames([...sorted]);
-        } else if (sort === "publisherDes") {
-            const sorted = gamesToSort.sort((a, b) => b.publisher.localeCompare(a.publisher));
-            setSortedGames([...sorted]);
-        } else if (sort === "developerAsc") {
-            const sorted = gamesToSort.sort((a, b) => a.developer.localeCompare(b.developer));
-            setSortedGames([...sorted]);
-        } else if (sort === "developerDes") {
-            const sorted = gamesToSort.sort((a, b) => b.developer.localeCompare(a.developer));
-            setSortedGames([...sorted]);
-        }
     }
 
     return (
@@ -87,21 +52,7 @@ export function Search(): ReactElement {
                     <>
                         <h1 className="search-result-text"> {`Found ${totalCount} game${searchResult.length > 1 ? "s" : ""}`} </h1>
                         <section className="show-pagination-toggle">
-                            <section id="games-sort">
-                                <label id="games-sort-label" htmlFor="games-sort-select"> Sort by: </label> 
-                                <select id="games-sort-select" name="games-sort-select" onChange={e => sortGames(e.target.value)}>
-                                    <optgroup className="games-sort-select__options">
-                                        <option value="titleAsc" defaultChecked> Title Ascending </option>
-                                        <option value="titleDes"> Title Descending </option>
-                                        <option value="playersAsc"> Players Ascending </option>
-                                        <option value="playersDes"> Players Descending </option>
-                                        <option value="publisherAsc"> Publisher Ascending </option>
-                                        <option value="publisherDes"> Publisher Descending </option>
-                                        <option value="developerAsc"> Developer Ascending </option>
-                                        <option value="developerDes"> Developer Descending </option>
-                                    </optgroup>
-                                </select>
-                            </section>
+                            <GameSorting games={searchResult} setSortedGames={setSearchResult}/>
                             
                             {searchResult.length > 80 ? <RangeSlider min={50} max={searchResult.length} setSliderValue={setNumberGamesShowing} /> : <></>}
 
@@ -110,9 +61,9 @@ export function Search(): ReactElement {
 
                         { 
                             gridView ?
-                                <GameGrid games={sortedGames.slice(0, numberGamesShowing)} />
+                                <GameGrid games={searchResult.slice(0, numberGamesShowing)} />
                                 :
-                                <GameList games={sortedGames.slice(0, numberGamesShowing)} />
+                                <GameList games={searchResult.slice(0, numberGamesShowing)} />
                         }
                     </>
                 :   <>
