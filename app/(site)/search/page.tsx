8f2a2515c7d7e2d1cbdc2eecfe425829234609a6
joelBit <joel.rollny@gmail.com>
2025-12-14 12:20:30 +0100

    Removed unnecessary section element

--- app/(site)/search/page.tsx
+++ app/(site)/search/page.tsx
@@ -15,53 +15,52 @@ export default async function SearchPage({ searchParams } : { searchParams: Prom
 
     return (
         <main id="searchPage">
-            <section id="searchPage-wrapper">
-                <search id="searchFilters">
-                    <h1 className="searchFilters__title"> Search Games </h1>
-                    <Form id="searchFilters__form" action="" scroll={false}>
-                        <article className="searchFilters__filters">
-                            <section className="selectSection">
-                                <h2 className="selectSection__title"> Category </h2>
-                    
-                                <select className="selectSection__select" name="category" defaultValue={params.category}>
-                                    { addAllOption(await getAllCategories()).map((element, index) => <option key={index} value={element}> {element} </option>) }
-                                </select>
-                            </section>
+            <search id="searchFilters">
+                <h1 className="searchFilters__title"> Search Games </h1>
 
-                            <section className="selectSection">
-                                <h2 className="selectSection__title"> Publisher </h2>
-                    
-                                <select className="selectSection__select" name="publisher" defaultValue={params.publisher}>
-                                    { addAllOption(await getAllPublishers()).map((element, index) => <option key={index} value={element}> {element} </option>) }
-                                </select>
-                            </section>
-                            
-                            <section className="selectSection">
-                                <h2 className="selectSection__title"> Developer </h2>
-                    
-                                <select className="selectSection__select" name="developer" defaultValue={params.developer}>
-                                    { addAllOption(await getAllDevelopers()).map((element, index) => <option key={index} value={element}> {element} </option>) }
-                                </select>
-                            </section>
-                        </article>
+                <Form id="searchFilters__form" action="" scroll={false}>
+                    <article className="searchFilters__filters">
+                        <section className="selectSection">
+                            <h2 className="selectSection__title"> Category </h2>
+                
+                            <select className="selectSection__select" name="category" defaultValue={params.category}>
+                                { addAllOption(await getAllCategories()).map((element, index) => <option key={index} value={element}> {element} </option>) }
+                            </select>
+                        </section>
 
-                        <search id="searchInput">
-                            <section className="input-wrapper">
-                                <span className="material-symbols-outlined"> search </span>
-                                <input 
-                                    id="searchTitle"
-                                    name="title"
-                                    type="text"
-                                    placeholder="Game Title"
-                                />
-                            </section>
+                        <section className="selectSection">
+                            <h2 className="selectSection__title"> Publisher </h2>
+                
+                            <select className="selectSection__select" name="publisher" defaultValue={params.publisher}>
+                                { addAllOption(await getAllPublishers()).map((element, index) => <option key={index} value={element}> {element} </option>) }
+                            </select>
+                        </section>
+                        
+                        <section className="selectSection">
+                            <h2 className="selectSection__title"> Developer </h2>
+                
+                            <select className="selectSection__select" name="developer" defaultValue={params.developer}>
+                                { addAllOption(await getAllDevelopers()).map((element, index) => <option key={index} value={element}> {element} </option>) }
+                            </select>
+                        </section>
+                    </article>
 
-                            <SearchButton />
-                        </search>
-                    </Form>
-                </search>
-                <Search />
-            </section>
+                    <search id="searchInput">
+                        <section className="input-wrapper">
+                            <span className="material-symbols-outlined"> search </span>
+                            <input 
+                                id="searchTitle"
+                                name="title"
+                                type="text"
+                                placeholder="Game Title"
+                            />
+                        </section>
+
+                        <SearchButton />
+                    </search>
+                </Form>
+            </search>
+            <Search />
         </main>
     );
 }
