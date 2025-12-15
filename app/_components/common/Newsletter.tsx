c72094a7e577db644e09274a77b1a64d715efccd
joelBit <joel.rollny@gmail.com>
2025-12-15 11:47:59 +0100

    Added messages to subscription

--- app/_components/common/Newsletter.tsx
+++ app/_components/common/Newsletter.tsx
@@ -1,8 +1,21 @@
-import { ReactElement } from "react";
+'use client';
+
+import { ReactElement, useActionState, useEffect, useState } from "react";
+import { createNewsletterSubscription } from "@/app/_actions/newsletter";
 
 import "./Newsletter.css";
 
 export function Newsletter(): ReactElement {
+    const [showMessage, setShowMessage] = useState<boolean>(false);
+    const [state, formAction] = useActionState(createNewsletterSubscription, { message: '', success: false });
+
+    useEffect(() => {
+        if (state?.message && !showMessage) {       // Show message for a fixed amount of time
+            setShowMessage(true);
+        }
+        setTimeout(() => setShowMessage(false), 5000);
+    }, [state]);
+        
     return (
         <section id="newsletter">
             <h2 className="newsletter-heading"> Newsletter </h2>
@@ -11,11 +24,20 @@ export function Newsletter(): ReactElement {
                 News, updates and exclusive goodies directly in your inbox.
             </p>
 
-            <input id="subscribeEmail" type="email" name="email" placeholder="Enter Email" required={true} autoComplete="off" />
+            <form id="newsletterSignup" action={formAction}>
+                <input id="subscribeEmail" type="email" name="email" placeholder="Enter Email" required={true} autoComplete="off" />
 
-            <button id="newsletterButton">
-                Subscribe 
-            </button>
+                <button id="newsletterButton">
+                    Subscribe 
+                </button>
+            </form>
+
+            {
+                showMessage ?
+                    <h2 className={state?.success ? "message-success" : "message-failure"}>
+                        {state?.message}
+                    </h2> : <></>
+            }
         </section>
     )
 }
