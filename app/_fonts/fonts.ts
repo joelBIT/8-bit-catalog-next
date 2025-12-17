78837db14127d8808b53a19ce75b31c7ba486868
joelBit <joel.rollny@gmail.com>
2025-12-17 09:21:08 +0100

    Removed unused fonts

--- app/_fonts/fonts.ts
+++ app/_fonts/fonts.ts
@@ -1,4 +1,4 @@
-import { Arima, Blinker, Irish_Grover, Rancho, Silkscreen, Single_Day, Iceland } from "next/font/google";
+import { Arima, Blinker, Irish_Grover, Rancho, Iceland } from "next/font/google";
 
 export const rancho = Rancho({
     subsets: ["latin"],
@@ -10,11 +10,6 @@ export const blinker = Blinker({
     weight: "400"
 });
 
-export const silkScreen = Silkscreen({
-    subsets: ["latin"],
-    weight: "400"
-});
-
 export const irishGrover = Irish_Grover({
     subsets: ["latin"],
     weight: "400"
@@ -22,10 +17,6 @@ export const irishGrover = Irish_Grover({
 
 export const arima = Arima({
     subsets: ["latin"],
-    weight: "400"
-});
-
-export const singleDay = Single_Day({
     weight: "400"
 });
 
