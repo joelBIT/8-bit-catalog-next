31eb302958f57471ea4613f144e590f979a26593
joelBit <joel.rollny@gmail.com>
2025-12-19 11:33:38 +0100

    Added unit test checking conversion of numbers to months

---
+++ __tests__/utils.test.tsx
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { getMonthText } from "@/app/_utils/utils";
+
+test('Convert numbers to corresponding months', () => {
+    expect(getMonthText(0)).toBe("January");
+    expect(getMonthText(1)).toBe("February");
+    expect(getMonthText(2)).toBe("March");
+    expect(getMonthText(3)).toBe("April");
+    expect(getMonthText(4)).toBe("May");
+    expect(getMonthText(5)).toBe("June");
+    expect(getMonthText(6)).toBe("July");
+    expect(getMonthText(7)).toBe("August");
+    expect(getMonthText(8)).toBe("September");
+    expect(getMonthText(9)).toBe("October");
+    expect(getMonthText(10)).toBe("November");
+    expect(getMonthText(11)).toBe("December");
+    expect(getMonthText(12)).toBe("");
+})
