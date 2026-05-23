1b127eb6f27c6ca92a682abafb60d1fb8951d4ff
joelBit <joel.rollny@gmail.com>
2026-05-23 12:22:20 +0200

    Fixed mobile view for landing cards

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -22,55 +22,6 @@ export default async function Home(): Promise<ReactElement> {
                     </h1>
 
                     <LandingCards allGameTitles={await getAllTitles()} />
-
-                    {/* <section className="landing-cards">
-                        <section className="landing-card">
-                            <h3 className="landing-title__number"> .01 </h3>
-                            <h3 className="landing-title__heading">Vision</h3>
-                            <p className="landing-card__text">
-                                A place for all things related to the Nintendo Entertainment System, where it is possible to 
-                                connect with other 8-bit enthusiasts.
-                            </p>
-
-                            <figure className="nes-figure">
-                                <Image 
-                                    src="/architecture/nesconsole.png" 
-                                    className="nes-image" 
-                                    alt="NES console" 
-                                    width={640} 
-                                    height={348} 
-                                    placeholder="blur"
-                                    blurDataURL="/architecture/nesconsole.png" 
-                                />
-                            </figure>
-                        </section>
-
-                        <section className="landing-card">
-                            <h3 className="landing-title__number"> .02 </h3>
-                            <h3 className="landing-title__heading">Solution</h3>
-                            <p className="landing-card__text">
-                                The 8-bit Catalog.
-                                This catalog is continuously updated with new information and functionality. 
-                                Test the catalog by entering a game title and clicking on the View button.
-                            </p>
-
-                            <section id="searchGameArea">
-                                <TitleSearch titles={await getAllTitles()} />
-                            </section>
-                        </section>
-
-                        <section className="landing-card">
-                            <h3 className="landing-title__number"> .03 </h3>
-                            <h3 className="landing-title__heading">Membership</h3>
-                            <p className="landing-card__text">
-                                Become a member to connect with other 8-bit enthusiasts.
-                                Membership unlock services including real-time interaction with 
-                                other members, exclusive offers, info about retro events, and more.
-                            </p>
-
-                            <Link href={URL_REGISTER_PAGE} className="signup-button"> Sign up now </Link>
-                        </section>
-                    </section> */}
                 </section>
             </section>
 
--- app/_components/home/LandingCards.tsx
+++ app/_components/home/LandingCards.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { LandingButton, TitleSearch } from ".";
@@ -9,23 +9,28 @@ import { URL_REGISTER_PAGE } from "@/app/_utils/utils";
 import "./LandingCards.css";
 
 export function LandingCards({allGameTitles}: {allGameTitles: string[]}): ReactElement {
-    let page = 1;
+    const [page, setPage] = useState<number>(1);
 
-    function setPage(pageNumber: number): void {
-        page = pageNumber;
+    let content = <LandingCardOne />;
+
+    if (page === 2) {
+        content = <LandingCardTwo />;
+    }
+
+    if (page === 3) {
+        content = <LandingCardThree />;
     }
 
     return (
         <section id="landingCards">
             <section className="card-navigation">
                 <LandingButton icon='chevron_left' onClick={() => setPage(((page - 1) % 4) === 0 ? 3 : ((page - 1) % 4))} />
-                <LandingButton icon='chevron_right' onClick={() => setPage(((page + 1) % 4) === 0 ? 1 : ((page + 1) % 4))} />    
-
                 <h3 className="landing-title__number">{page}/3</h3>
+                <LandingButton icon='chevron_right' onClick={() => setPage(((page + 1) % 4) === 0 ? 1 : ((page + 1) % 4))} />
             </section>
 
             <section className="landing-cards">
-
+                {content}
             </section>
         </section>
     );
@@ -33,24 +38,28 @@ export function LandingCards({allGameTitles}: {allGameTitles: string[]}): ReactE
     function LandingCardOne(): ReactElement {
         return (
             <section className="landing-card">
-                <h3 className="landing-title__number"> .01 </h3>
-                <h3 className="landing-title__heading">Vision</h3>
-                <p className="landing-card__text">
-                    A place for all things related to the Nintendo Entertainment System, where it is possible to 
-                    connect with other 8-bit enthusiasts.
-                </p>
+                <section className="landing-details">
+                    <h3 className="landing-title__number"> .01 </h3>
+                    <h3 className="landing-title__heading">Vision</h3>
+                    <p className="landing-card__text">
+                        A place for all things related to the Nintendo Entertainment System, where it is possible to 
+                        connect with other 8-bit enthusiasts.
+                    </p>
+                </section>
 
-                <figure className="nes-figure">
-                    <Image 
-                        src="/architecture/nesconsole.png" 
-                        className="nes-image" 
-                        alt="NES console" 
-                        width={640} 
-                        height={348} 
-                        placeholder="blur"
-                        blurDataURL="/architecture/nesconsole.png" 
-                    />
-                </figure>
+                <section className="landing-content">
+                    <figure className="nes-figure">
+                        <Image 
+                            src="/architecture/nesconsole.png" 
+                            className="nes-image" 
+                            alt="NES console" 
+                            width={640} 
+                            height={348} 
+                            placeholder="blur"
+                            blurDataURL="/architecture/nesconsole.png" 
+                        />
+                    </figure>
+                </section>
             </section>
         );
     }
@@ -58,16 +67,20 @@ export function LandingCards({allGameTitles}: {allGameTitles: string[]}): ReactE
     function LandingCardTwo(): ReactElement {
         return (
             <section className="landing-card">
-                <h3 className="landing-title__number"> .02 </h3>
-                <h3 className="landing-title__heading">Solution</h3>
-                <p className="landing-card__text">
-                    The 8-bit Catalog.
-                    This catalog is continuously updated with new information and functionality. 
-                    Test the catalog by entering a game title and clicking on the View button.
-                </p>
+                <section className="landing-details">
+                    <h3 className="landing-title__number"> .02 </h3>
+                    <h3 className="landing-title__heading">Solution</h3>
+                    <p className="landing-card__text">
+                        The 8-bit Catalog.
+                        This catalog is continuously updated with new information and functionality. 
+                        Test the catalog by entering a game title and clicking on the View button.
+                    </p>
+                </section>
 
-                <section id="searchGameArea">
-                    <TitleSearch titles={allGameTitles} />
+                <section className="landing-content">
+                    <section id="searchGameArea">
+                        <TitleSearch titles={allGameTitles} />
+                    </section>
                 </section>
             </section>
         );
@@ -76,15 +89,19 @@ export function LandingCards({allGameTitles}: {allGameTitles: string[]}): ReactE
     function LandingCardThree(): ReactElement {
         return (
             <section className="landing-card">
-                <h3 className="landing-title__number"> .03 </h3>
-                <h3 className="landing-title__heading">Membership</h3>
-                <p className="landing-card__text">
-                    Become a member to connect with other 8-bit enthusiasts.
-                    Membership unlock services including real-time interaction with 
-                    other members, exclusive offers, info about retro events, and more.
-                </p>
+                <section className="landing-details">
+                    <h3 className="landing-title__number"> .03 </h3>
+                    <h3 className="landing-title__heading">Membership</h3>
+                    <p className="landing-card__text">
+                        Become a member to connect with other 8-bit enthusiasts.
+                        Membership unlock services including real-time interaction with 
+                        other members, exclusive offers, info about retro events, and more.
+                    </p>
+                </section>
 
-                <Link href={URL_REGISTER_PAGE} className="signup-button"> Sign up now </Link>
+                <section className="landing-content">
+                    <Link href={URL_REGISTER_PAGE} className="signup-button"> Sign up now </Link>
+                </section>
             </section>
         );
     }
