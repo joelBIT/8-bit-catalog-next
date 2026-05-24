05f671f8971949991097bdf37923c87f13d836b5
joelBit <joel.rollny@gmail.com>
2026-05-24 13:39:52 +0200

    Created ContactForm component

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import { ContactForm } from "@/app/_components/common";
 
 import "./page.css";
 
@@ -9,7 +10,25 @@ export default function ContactPage(): ReactElement {
 
     return (
         <main id="contactPage">
+            <section className="contact-details">
+                <h2 className="contact-heading">Get in touch</h2>
+                <p className="contact-text">
+                    Please contact me if you have any questions or wishes about the 8-bit catalog. 
+                    I will get back to you as soon as possible.
+                </p>
+            </section>
 
+            <section className="contact-form">
+                <ContactForm />
+            </section>
         </main>
     );
+
+    function ContactCard({icon, title, text}: {icon: string, title: string, text: string}): ReactElement {
+        return (
+            <section className="contactCard">
+
+            </section>
+        );
+    }
 }
---
+++ app/_components/common/ContactForm.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { type ReactElement, useActionState } from "react";
+
+import "./ContactForm.css";
+
+export function ContactForm(): ReactElement {
+    const [ state, formAction ] = useActionState(sendMessage,  null);
+
+    function sendMessage(): void {
+        console.log("sent");
+        console.log(state);
+    }
+    
+    return (
+        <form id="contactForm" action={formAction}>
+            <section className="information-input">
+                <label className="input-label" htmlFor="name">
+                    Name
+                </label>
+
+                <input 
+                    id="name"
+                    name="name"
+                    type="text"
+                    className={`input-field`}
+                    autoComplete="none" 
+                />
+            </section>
+
+            <section className="information-input">
+                <label className="input-label required" htmlFor="email">
+                    Email
+                </label>
+
+                <input 
+                    id="email"
+                    name="email" 
+                    type="email"
+                    className={`input-field`}
+                    autoComplete="off" 
+                    required 
+                />
+            </section>
+
+            <section className="information-input">
+                <label className="input-label" htmlFor="subject">
+                    Subject
+                </label>
+
+                <input 
+                    id="subject"
+                    name="subject"
+                    type="text"
+                    className={`input-field`}
+                    autoComplete="none" 
+                />
+            </section>
+
+            <section className="information-input">
+                <label className="input-label required" htmlFor="message">
+                    Message
+                </label>
+
+                <textarea id="message" name="message" className={`input-field`} required />
+            </section>
+
+            <button id="sendButton" className="sendButton" type="submit">
+                <span> Send </span>
+            </button>
+        </form>
+    );
+}
