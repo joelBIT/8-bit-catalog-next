4a5f324be8942a936e7f37c7d1a416c11b02c67e
joelBit <joel.rollny@gmail.com>
2026-05-24 16:08:46 +0200

    Styled contact cards

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import Link from "next/link";
 import { ContactForm } from "@/app/_components/common";
 
 import "./page.css";
@@ -7,6 +8,12 @@ import "./page.css";
  * This page contains contact information.
  */
 export default function ContactPage(): ReactElement {
+
+    const CONTACT = [
+        {icon: 'mail', title: 'Email', text: 'contact@joel-rollny.eu', link: 'mailto:contact@joel-rollny.eu'},
+        {icon: 'location_on', title: 'Location', text: 'Karlstad, Sweden', link: 'https://maps.app.goo.gl/gDzYq4uLUqTVSyV8A'},
+        {icon: 'globe', title: 'Website', text: 'http://www.joel-rollny.eu', link: 'http://www.joel-rollny.eu'}
+    ]
 
     return (
         <main id="contactPage">
@@ -21,6 +28,8 @@ export default function ContactPage(): ReactElement {
                         Please contact me if you have any questions or wishes about the 8-bit catalog. 
                         I will get back to you as soon as possible.
                     </p>
+
+                    {CONTACT.map(contact => <ContactCard icon={contact.icon} title={contact.title} text={contact.text} link={contact.link} />)}
                 </section>
 
                 <section className="contact-form">
@@ -31,10 +40,19 @@ export default function ContactPage(): ReactElement {
         </main>
     );
 
-    function ContactCard({icon, title, text}: {icon: string, title: string, text: string}): ReactElement {
+    function ContactCard({icon, title, text, link}: {icon: string, title: string, text: string, link: string}): ReactElement {
         return (
             <section className="contactCard">
+                <section className="contact-icon material-symbols-outlined">{icon}</section>
+                
+                <section className="contact-info">
+                    <h2 className="contact-info__heading"> {title} </h2>
+                    <p className="contact-info__text"> {text} </p>
+                </section>
 
+                <Link className="contact-link" target="_blank" href={link}>
+                    <span className="material-symbols-outlined"> open_in_new </span>
+                </Link> 
             </section>
         );
     }
