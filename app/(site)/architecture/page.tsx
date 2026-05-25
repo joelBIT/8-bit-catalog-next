b543af2d114f56f075ae93a5dab4b56e5a4d4353
joelBit <joel.rollny@gmail.com>
2026-05-25 09:08:19 +0200

    Changed main styling of the page

--- app/(site)/architecture/page.tsx
+++ app/(site)/architecture/page.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from "react";
-import Image from "next/image";
 import { ArticleCard } from "@/app/_components/common";
 import { Article } from "@/app/_types/types";
 import { getAllArticles } from "@/app/_db/articles-db";
@@ -10,50 +9,35 @@ export default async function ArchitecturePage(): Promise<ReactElement> {
     let articles: Article[] = [];
     
     try {
-        articles = await getAllArticles();
+        if (!articles.length) {
+            articles = await getAllArticles();
+        }
     } catch (error) {
         console.log(error);
     }
 
     return (
         <main id="architecturePage">
-            <section id="nes-introduction">
-                <section className="nes-introduction__content">
-                    <h3 className="nes-introduction__subheading"> Introducing the </h3>
-                    <h2 className="nes-introduction__heading"> Nintendo <i>Entertainment</i> System </h2>
-                </section>
+            <h1 className="architecture-title"> Architecture </h1>
 
-                <figure className="architecture-figure">
-                    <Image 
-                        src="/architecture/nesconsole.png" 
-                        className="architecture-image" 
-                        alt="NES console" 
-                        width={640} 
-                        height={348} 
-                        placeholder="blur"
-                        blurDataURL="/architecture/nesconsole.png" 
-                    />
-                </figure>
-            </section>
-
-            <section className="nintendo-presentation">
-                <h2 className="nintendo-presentation__heading"> There's something about the NES? </h2>
-                <p className="nintendo-presentation__text">
-                    It is regarded as one of the most influential gaming consoles.
-                    The NES pioneered a now-standard business model of licensing third-party developers to produce and distribute games.
-                    It was released in Japan on July 15, 1983, as the Family Computer (Famicom). Today, the NES is the 14th best-selling console 
-                    of all time and has a thriving community.
-                    The console has 1,377 licensed games globally, and the community has produced many unofficial games.
-                    <br /><br />
-                    The Nintendo Entertainment System has the following components: 2A03 CPU based on the MOS Technology 6502 8-bit microprocessor, 
-                    2C02 PPU (picture processing unit), serial input for game controllers, audio output comprising four tone generators and a delta 
-                    modulation playback device, 2 KiB of RAM for use by the CPU and 2 KiB of RAM for use by the PPU.
-                    <br /><br />
-                    The NES has two general-purpose controller ports on the front of the console, as well as a (rarely used) 48-pin expansion port 
-                    underneath. The Famicom's standard controllers are hardwired to the front of the unit, and a special 15-pin expansion port 
-                    is commonly used for third-party controllers. The AV Famicom, however, features detachable controllers using the same 
-                    ports as the NES. The NES and Famicom have a set of I/O ports used for controllers and other peripherals.
-                </p>
+            <section id="architecture-introduction">
+                <h2 className="architecture-heading">NES Architecture</h2>
+                    <p className="architecture-text">
+                        It is regarded as one of the most influential gaming consoles.
+                        The NES pioneered a now-standard business model of licensing third-party developers to produce and distribute games.
+                        It was released in Japan on July 15, 1983, as the Family Computer (Famicom). Today, the NES is the 14th best-selling console 
+                        of all time and has a thriving community.
+                        The console has 1,377 licensed games globally, and the community has produced many unofficial games.
+                        <br /><br />
+                        The Nintendo Entertainment System has the following components: 2A03 CPU based on the MOS Technology 6502 8-bit microprocessor, 
+                        2C02 PPU (picture processing unit), serial input for game controllers, audio output comprising four tone generators and a delta 
+                        modulation playback device, 2 KiB of RAM for use by the CPU and 2 KiB of RAM for use by the PPU.
+                        <br /><br />
+                        The NES has two general-purpose controller ports on the front of the console, as well as a (rarely used) 48-pin expansion port 
+                        underneath. The Famicom's standard controllers are hardwired to the front of the unit, and a special 15-pin expansion port 
+                        is commonly used for third-party controllers. The AV Famicom, however, features detachable controllers using the same 
+                        ports as the NES. The NES and Famicom have a set of I/O ports used for controllers and other peripherals.
+                    </p>
             </section>
 
             <iframe 
