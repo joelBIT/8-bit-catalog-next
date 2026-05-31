a65da854f82974e34d58b47ea3158deeddaebd16
joelBit <joel.rollny@gmail.com>
2026-05-31 13:24:31 +0200

    Changed styling of news card

--- app/_components/home/NewsCard.tsx
+++ app/_components/home/NewsCard.tsx
@@ -1,8 +1,7 @@
 import { ReactElement } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { News } from "@/app/_db/schema/news";
-import { adjustTextLength, URL_NEWS_PAGE } from "@/app/_utils/utils";
+import { URL_NEWS_PAGE } from "@/app/_utils/utils";
 
 import "./NewsCard.css";
 
@@ -12,18 +11,28 @@ import "./NewsCard.css";
 export function NewsCard({ news }: { news: News }): ReactElement {
 
     return (
-        <section className="news-card">
-            <Image src={`/news/${news.image}`} unoptimized={true} alt="News Card image" className="news-card-image" width={200} height={200} quality={100} />
-            <div className="darken-image-right" />
+        <section className="newsCard">
+            <Link href={URL_NEWS_PAGE + `/${news.id}`} className="news-image-link" title={`News about ${news.heading}`}>
+                <article className="news-image-container">
+                    <img src={`/news/${news.image}`} alt="News Card image" className="news-image" />
+                </article>
+            </Link>
 
-            <section className="news-card-text">
-                <section className="news-card-title">
-                    <h2 className="news-heading"> {news.heading} </h2>
-                    <h2 className="news-date"> {news.published?.toString()} </h2>
-                </section>
+            <section className="content">
+                <h1 className="content-title">{ news.heading }</h1>
 
-                <h2 className="news-text"> {adjustTextLength(news.text, 130)} </h2>
-                <Link href={URL_NEWS_PAGE + `/${news.id}`} className="navigation-link"> Read More </Link>
+                <p className="news-date">
+                    <span className="material-symbols-outlined"> calendar_month </span>
+                    {new Date(news.published).toDateString()} 
+                </p>
+
+                <p className="content-text">{ news.text.slice(0, 100) + "..." }</p>
+            </section>
+            
+            <section className="news-bottom">
+                <Link href={URL_NEWS_PAGE + `/${news.id}`} className="news-link">
+                    READ MORE
+                </Link>
             </section>
         </section>
     );
