25ee33566bde0576cdfad49b3fcf88cdf5ff3e59
joelBit <joel.rollny@gmail.com>
2026-07-16 17:33:56 +0200

    Moved message inside of EditProfileForm

--- app/_components/account/forms/EditProfileForm.tsx
+++ app/_components/account/forms/EditProfileForm.tsx
@@ -27,79 +27,77 @@ export function EditProfileForm({ profile }: { profile: Profile }): ReactElement
     }, [state]);
 
     return (
-        <>
-            <form id="profileForm" action={formAction}>
-                <section className="information-input">
-                    <label className="input-label" htmlFor="first_name">
-                        First name
-                    </label>
+        <form id="profileForm" action={formAction}>
+            <section className="information-input">
+                <label className="input-label" htmlFor="first_name">
+                    First name
+                </label>
 
-                    <input 
-                        id="first_name"
-                        name="first_name"
-                        type="text"
-                        className="input-field"
-                        autoComplete="none"
-                        defaultValue={state.firstName ?? ''} 
-                    />
-                </section>
+                <input 
+                    id="first_name"
+                    name="first_name"
+                    type="text"
+                    className="input-field"
+                    autoComplete="none"
+                    defaultValue={state.firstName ?? ''} 
+                />
+            </section>
 
-                <section className="information-input">
-                    <label className="input-label" htmlFor="last_name">
-                        Last name
-                    </label>
+            <section className="information-input">
+                <label className="input-label" htmlFor="last_name">
+                    Last name
+                </label>
 
-                    <input 
-                        id="last_name"
-                        name="last_name"
-                        type="text"
-                        className="input-field"
-                        autoComplete="none"
-                        defaultValue={state.lastName ?? ''} 
-                    />
-                </section>
+                <input 
+                    id="last_name"
+                    name="last_name"
+                    type="text"
+                    className="input-field"
+                    autoComplete="none"
+                    defaultValue={state.lastName ?? ''} 
+                />
+            </section>
 
-                <section className="information-input">
-                    <label className="input-label" htmlFor="bio">
-                        Bio
-                    </label>
+            <section className="information-input">
+                <label className="input-label" htmlFor="bio">
+                    Bio
+                </label>
 
-                    <textarea 
-                        id="bio"
-                        name="bio" 
-                        className="input-field edit-profile__bio"
-                        defaultValue={state.bio ?? ''} 
-                        placeholder="About me" 
-                    />
-                </section>
+                <textarea 
+                    id="bio"
+                    name="bio" 
+                    className="input-field edit-profile__bio"
+                    defaultValue={state.bio ?? ''} 
+                    placeholder="About me" 
+                />
+            </section>
 
-                <section className="input">
-                    <input 
-                        id="birth_date"
-                        name="birth_date" 
-                        type="date"
-                        max={new Date().toLocaleDateString('en-ca')}
-                        defaultValue={`${state.birthDate?.toLocaleDateString('en-ca')}`}
-                        className="form__field"
-                    />
+            <section className="input">
+                <input 
+                    id="birth_date"
+                    name="birth_date" 
+                    type="date"
+                    max={new Date().toLocaleDateString('en-ca')}
+                    defaultValue={`${state.birthDate?.toLocaleDateString('en-ca')}`}
+                    className="form__field"
+                />
 
-                    <span className="form__field-label">
-                        Date of Birth
-                    </span>
-                </section>
+                <span className="form__field-label">
+                    Date of Birth
+                </span>
+            </section>
 
-                <section className="information-input">
-                    <label className="input-label" htmlFor="phone">
-                        Phone number
-                    </label>
+            <section className="information-input">
+                <label className="input-label" htmlFor="phone">
+                    Phone number
+                </label>
 
-                    <PhoneInput country={state.success ? "se" : undefined} inputProps={{name: 'phone', autoComplete: "none"}} />
-                </section>
-                
-                <button className="authButton" type="submit">
-                    <span className="authButton__text"> Save </span>
-                </button>
-            </form>
+                <PhoneInput country={state.success ? "se" : undefined} inputProps={{name: 'phone', autoComplete: "none"}} />
+            </section>
+            
+            <button className="authButton" type="submit">
+                <span className="authButton__text"> Save </span>
+            </button>
 
             { 
                 showMessage ? 
@@ -107,6 +105,6 @@ export function EditProfileForm({ profile }: { profile: Profile }): ReactElement
                         {state?.message}
                     </h2> : <></> 
             }
-        </>
+        </form>
     );
 }
