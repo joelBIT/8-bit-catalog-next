e9c9be0ea5f455b22a3af23e772f0368703ace6c
joelBit <joel.rollny@gmail.com>
2026-07-29 11:48:58 +0200

    Fixed news image and news text

--- app/(site)/news/[id]/page.tsx
+++ app/(site)/news/[id]/page.tsx
@@ -29,67 +29,63 @@ export default async function NewsPage({params}: {params: Promise<{ id: string }
 
     return (
         <main id="newsPage">
-            <section className="news-body">
-                <Link href={URL_HOME} className="back-link">
-                    <span className="material-symbols-outlined"> arrow_back_ios </span> 
-                    <h1 className="back-link__text"> Back to start page </h1>
-                </Link>
+            <figure id="news-figure">
+                <Image 
+                    src={`${URL_NEWS_PAGE}/${news.image}`} 
+                    className="news-image" 
+                    alt="News Image" 
+                    width={1228} 
+                    height={480} 
+                    loading="eager" 
+                    placeholder="blur"
+                    blurDataURL={`${URL_NEWS_PAGE}/${news.image}`}
+                />
+            </figure>
 
-                <section id="news-top">
-                    <Image 
-                        src={`${URL_NEWS_PAGE}/${news.image}`} 
-                        className="news-top-image" 
-                        alt="News Image" 
-                        width={1228} 
-                        height={480} 
-                        loading="eager" 
-                        placeholder="blur"
-                        blurDataURL={`${URL_NEWS_PAGE}/${news.image}`}
-                    />
-                </section>
-
-                <section id="news-bottom">
-                    <article id="top-news">
-                        <h2 className="top-news-heading"> Top News </h2>
-
-                        {
-                            (await getTopNews()).map((news: News, index: number) => 
-                                <section className="top-news-list" key={news.id}>
-                                    <Link 
-                                        href={URL_NEWS_PAGE + `/${news.id}`} 
-                                        className={`top-news-list__heading ${parseInt(id) == news.id ? "disabled-link" : ""}`}
-                                    >
-                                        {news.heading}
-                                    </Link>
-                                    
-                                    <article className="top-news-list__calendar">
-                                        <span className="material-symbols-outlined"> calendar_month </span>
-                                        <h3 className="top-news-published">{new Date(news.published).toDateString()}</h3>
-                                    </article>
-
-                                    <h2 className="top-news-list-number">{"0" + (index + 1)}</h2>
-                                </section>
-                            )
-                        }
-                    </article>
-
-                    <article id="news-text">
-                        <h2 className="news-heading"> {news.heading} </h2>
-                        <section className="news-text-author">
-                            <article title="Author">
-                                <span className="material-symbols-outlined article-person"> article_person </span>
-                                <h3 className="news-author"> {news.author} </h3>
-                            </article>
-
-                            <article title="Published">
-                                <span className="material-symbols-outlined"> calendar_month </span>
-                                <h3 className="news-published"> {new Date(news.published).toDateString()} </h3>
-                            </article>
-                        </section>
-                        
+            <section className="news-content">
+                <article id="news-article">
+                    <h2 className="news-heading"> {news.heading} </h2>
+                    
+                    <p className="news-text">
                         {news.text}
-                    </article>
-                </section>
+                    </p>
+                </article>
+
+                <article id="top-news">
+                    <section className="news-text-author">
+                        <article title="Author">
+                            <span className="material-symbols-outlined article-person"> article_person </span>
+                            <h3 className="news-author"> {news.author} </h3>
+                        </article>
+
+                        <article title="Published">
+                            <span className="material-symbols-outlined"> calendar_month </span>
+                            <h3 className="news-published"> {new Date(news.published).toDateString()} </h3>
+                        </article>
+                    </section>
+
+                    <h2 className="top-news-heading"> Top News </h2>
+
+                    {
+                        (await getTopNews()).map((news: News, index: number) => 
+                            <section className="top-news-list" key={news.id}>
+                                <Link 
+                                    href={URL_NEWS_PAGE + `/${news.id}`} 
+                                    className={`top-news-list__heading ${parseInt(id) == news.id ? "disabled-link" : ""}`}
+                                >
+                                    {news.heading}
+                                </Link>
+                                
+                                <article className="top-news-list__calendar">
+                                    <span className="material-symbols-outlined"> calendar_month </span>
+                                    <h3 className="top-news-published">{new Date(news.published).toDateString()}</h3>
+                                </article>
+
+                                <h2 className="top-news-list-number">{"0" + (index + 1)}</h2>
+                            </section>
+                        )
+                    }
+                </article>
             </section>
 
             <div className="darken-image-bottom" />
