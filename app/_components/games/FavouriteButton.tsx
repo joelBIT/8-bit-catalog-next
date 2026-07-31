e745a381960e472a88d39bd1fdef5334e217eb34
joelBit <joel.rollny@gmail.com>
2026-07-31 08:54:20 +0200

    Created favourite button

---
+++ app/_components/games/FavouriteButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ReactElement } from "react";
+import { useFavourites } from "@/app/_hooks";
+import { Game } from "@/app/_db/schema/games";
+
+import "./FavouriteButton.css";
+
+export function FavouriteButton({game}: {game: Game}): ReactElement {
+    const { addFavouriteGame, removeFavouriteGame, isFavourite } = useFavourites();
+    const favourite = isFavourite(game.id);
+
+    if (favourite) {
+        return (
+            <section className="favouriteButton" onClick={() => removeFavouriteGame(game)}>
+                <span className="material-symbols-outlined">do_not_disturb_on</span>
+                <span className="button-text">Remove from collection</span>
+            </section>
+        );
+    }
+
+    return (
+        <section className="favouriteButton" onClick={() => addFavouriteGame(game)}>
+            <span className="material-symbols-outlined">add_circle</span>
+            <span className="button-text">Add to collection</span>
+        </section>
+    );
+}
